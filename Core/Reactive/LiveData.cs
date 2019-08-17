8ca1606beee818e81c942d828b18b8a451412cf4
Felipe de Souza Longo <felipeslongo@gmail.com>
2019-08-17 20:38:54 -0300

    Prototype implementation of LiveData

--- Core/Reactive/LiveData.cs
+++ Core/Reactive/LiveData.cs
@@ -1,7 +1,55 @@
+using System;
+
 namespace Core.Reactive
 {
-    public class LiveData
+    /// <summary>
+    /// LiveData is a data holder class that can be observed.
+    /// Use it to create observable properties in C#.
+    /// </summary>
+    /// <remarks>
+    /// Inspired by the LiveData class in Android Architecture Components
+    /// </remarks>
+    /// <typeparam name="T">Wrapped type</typeparam>
+    /// <see cref="https://developer.android.com/topic/libraries/architecture/livedata"/>
+    /// <see cref="https://developer.android.com/reference/androidx/lifecycle/LiveData.html"/>
+    public class LiveData<T>
     {
+        private T _value;
+        private event EventHandler _propertyChanged;
+
+        public LiveData(T value)
+        {
+            Value = value;
+        }
+
+        public LiveData() : this(default)
+        {
+        }
         
+        public event EventHandler PropertyChanged
+        {
+            add
+            {
+                value(this, EventArgs.Empty);
+                _propertyChanged += value;
+            }
+            remove => _propertyChanged -= value;
+        }
+
+        public T Value
+        {
+            get => _value;
+            set => SetValue(value);
+        }
+
+        private void SetValue(T value)
+        {
+            _value = value;
+            OnPropertyChanged();
+        }
+
+        public static implicit operator T(LiveData<T> liveData) => liveData.Value;
+
+        protected virtual void OnPropertyChanged() => _propertyChanged?.Invoke(this, EventArgs.Empty);
     }
 }
