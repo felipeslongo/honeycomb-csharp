d5b85ac487879beaccd76d6da006fcb8a4a6b8aa
Felipe de Souza Longo <felipeslongo@gmail.com>
2019-08-17 21:42:18 -0300

    rename MutableLiveDataTests to LiveDataTests

---
+++ CoreTests/Reactive/LiveDataTests.cs
@@ -0,0 +1,40 @@
+using System;
+using Core.Reactive;
+using Core.Strings;
+using Xunit;
+
+namespace CoreTests.Reactive
+{
+    public class LiveDataTests
+    {
+        private readonly object _sameValue = new object();
+        private readonly object _differentValue = new object();
+
+        public class ValueTests : LiveDataTests
+        {
+            [Fact]
+            public void GivenAInitialValue_ShouldRaisePropertyChanged_WhenValueSetIsNotEquals()
+            {
+                var liveData = new MutableLiveData<object>(_sameValue);
+
+                Assert.Raises<EventArgs>(
+                    handler => liveData.PropertyChanged += handler,
+                    handler => liveData.PropertyChanged -= handler,
+                    () => liveData.Value = _differentValue
+                );
+            }
+
+            [Fact]
+            public void GivenAInitialValue_ShouldNotRaisePropertyChanged_WhenValueSetIsEquals()
+            {
+                var liveData = new MutableLiveData<object>(_sameValue);
+                var eventWasRaised = false;
+                liveData.PropertyChanged += (sender, args) => eventWasRaised = true;
+
+                liveData.Value = _sameValue;
+
+                Assert.False(eventWasRaised);
+            }
+        }
+    }
+}
