06dd9d0e98486d87a1ed29a4241d4486350410e1
Felipe de Souza Longo <felipeslongo@gmail.com>
2019-08-21 00:37:56 -0300

    Field.GetSetter created

---
+++ CoreTests/Reflection/FieldTests.cs
@@ -0,0 +1,38 @@
+﻿using Core.Reflection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace CoreTests.Reflection
+{
+    public class FieldTests
+    {
+        private int _privateField;
+        private readonly int _privateReadonlyField = 0;
+
+        public class GetSetterTests: FieldTests
+        {
+            [Fact]
+            [Trait(nameof(Category), Category.Unit)]
+            public void GivenAPrivateField_ShouldBeSetWithThePassedValue_WhenSetterInvoked()
+            {
+                var value = 1;
+                var setter = Field.GetSetter(this, @this => @this._privateField);
+
+                setter(value);
+
+                Assert.Equal(value, _privateField);
+            }
+
+            [Fact]
+            [Trait(nameof(Category), Category.Unit)]
+            public void GivenAPrivateReadonlyField_ShouldThrowException_WhenGetSetterCalled()
+            {
+                var exception = Assert.Throws<ArgumentException>(() => Field.GetSetter(this, @this => @this._privateReadonlyField));
+
+                Assert.Contains(Field.MessageFieldIsReadonly, exception.Message);
+            }
+        }
+    }
+}
