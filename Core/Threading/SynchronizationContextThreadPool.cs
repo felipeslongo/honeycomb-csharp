7ee156991a158c581012f202fceb0b57542a49f0
Felipe de Souza Longo <felipeslongo@gmail.com>
2019-08-25 11:21:52 -0300

    Create SynchronizationContextThreadPool.cs

---
+++ Core/Threading/SynchronizationContextThreadPool.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace Core.Threading
+{
+    /// <summary>
+    /// <see cref="SynchronizationContext"/> implementation that delegates execution to a ThreadPool Thread using <see cref="Task.Run(System.Action)"/>.
+    /// Mainly used to UnitTest porpuses.
+    /// </summary>
+    public class SynchronizationContextThreadPool : SynchronizationContext
+    {
+        public override void Post(SendOrPostCallback d, object state)
+        {
+            Task.Run(() => d(state));
+        }
+
+        public override void Send(SendOrPostCallback d, object state)
+        {
+            Task.Run(() => d(state)).Wait();
+        }
+    }
+}
