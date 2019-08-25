5edf26378c434176422b08148dae1358c05d69c1
Felipe de Souza Longo <felipeslongo@gmail.com>
2019-08-25 11:47:59 -0300

    Create SynchronizationContextCurrentThread.cs

---
+++ Core/Threading/SynchronizationContextCurrentThread.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading;
+
+namespace Core.Threading
+{
+    /// <summary>
+    /// <see cref="SynchronizationContext"/> implementation that is dumb and always executes synchronously in the current <see cref="Thread"/>/>.
+    /// Mainly used to UnitTest porpuses.
+    /// </summary>
+    public class SynchronizationContextCurrentThread : SynchronizationContext
+    {
+        public override void Post(SendOrPostCallback d, object state) => Send(d, state);
+        public override void Send(SendOrPostCallback d, object state) => d(state);
+    }
+}
