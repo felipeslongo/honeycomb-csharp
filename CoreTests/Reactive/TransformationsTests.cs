8ec62dbfeb4342d30d502655bc77122e931e160c
Felipe de Souza Longo <felipeslongo@gmail.com>
2019-09-08 02:04:41 -0300

    create Transformations.FromEventPattern tests

--- CoreTests/Reactive/TransformationsTests.cs
+++ CoreTests/Reactive/TransformationsTests.cs
@@ -1,7 +1,9 @@
 ﻿using Core.Reactive;
+using CoreTests.Assertions;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
 using Xunit;
 
 namespace CoreTests.Reactive
@@ -23,6 +25,73 @@ namespace CoreTests.Reactive
                 source.Value = DifferentValue;
 
                 Assert.Equal(DifferentValue.ToString(), returned.Value);
+            }
+        }
+
+        public class FromEventPatternTests: TransformationsTests
+        {
+            public event EventHandler<EventArgs> EventHandler;
+
+            [Fact]
+            [Trait(nameof(Category), Category.Unit)]
+            public void GivenAnFromEventPatternLiveData_ShouldCaptureTheEventArg_WhenEventHandlerIsInvoked()
+            {
+                var returned = Transformations.FromEventPattern<EventArgs>(handler => EventHandler += handler, handler => EventHandler -= handler);
+                var expected = EventArgs.Empty;
+
+                EventHandler(this, expected);
+
+                Assert.Equal(expected, returned.Value);
+            }
+
+            [Fact]
+            [Trait(nameof(Category), Category.Unit)]
+            public void GivenAnFromEventPatternLiveData_ShouldCaptureTheLastInvocationEventArg_WhenEventHandlerIsInvokedMultipleTimes()
+            {
+                var returned = Transformations.FromEventPattern<EventArgs>(handler => EventHandler += handler, handler => EventHandler -= handler);
+                var firstEventArgs = EventArgs.Empty;
+                var lastEventArgs = new EventArgs();
+                EventHandler(this, firstEventArgs);
+
+                EventHandler(this, lastEventArgs);
+
+                Assert.Equal(lastEventArgs, returned.Value);
+            }
+
+            [Fact]
+            [Trait(nameof(Category), Category.Unit)]
+            public void GivenAnFromEventPatternLiveData_ShouldSubscribeIntoTheEventHandler_WhenCreated()
+            {
+                var eventHandlerMock = new ActionMock();
+
+                var returned = Transformations.FromEventPattern<EventArgs>(handler => eventHandlerMock.MockedAction(), _ => { });
+
+                Assert.True(eventHandlerMock.IsInvoked);
+            }
+
+            [Fact]
+            [Trait(nameof(Category), Category.Unit)]
+            public void GivenAnFromEventPatternLiveData_ShouldUnsubscribeFromTheEventHandler_WhenDisposed()
+            {
+                var eventHandlerMock = new ActionMock();
+                var returned = Transformations.FromEventPattern<EventArgs>(handler => EventHandler += handler, _ => eventHandlerMock.MockedAction());
+
+                returned.Dispose();
+
+                Assert.True(eventHandlerMock.IsInvoked);
+            }
+
+            [Fact]
+            [Trait(nameof(Category), Category.Unit)]
+            public void GivenAnFromEventPatternLiveData_ShouldUnsubscribeIntoTheCapturedSynchronizationContext_WhenDisposed()
+            {
+                var context = new SynchronizationContextMock();
+                SynchronizationContext.SetSynchronizationContext(context);
+                var returned = Transformations.FromEventPattern<EventArgs>(handler => EventHandler += handler, handler => EventHandler -= handler);
+
+                returned.Dispose();
+
+                Assert.True(context.PostWasCalled);
             }
         }
     }
