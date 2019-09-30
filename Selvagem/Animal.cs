3de320f09fc6bddb3ef3b04657d2f0bcc02f62f7
Felipe de Souza Longo <felipeslongo@gmail.com>
2019-09-30 00:47:29 -0300

    Criando animais

---
+++ Selvagem/Animal.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Selvagem
+{
+    public abstract class Animal
+    {
+        public Guid Id { get; } = Guid.NewGuid();
+    }
+}
