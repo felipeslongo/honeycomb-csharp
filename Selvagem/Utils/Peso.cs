fe3fe797cf54a431fffb3dc7a512fe3947538d8f
Felipe de Souza Longo <felipeslongo@gmail.com>
2019-10-01 18:56:00 -0300

    Implicit e Explicit operator

--- Selvagem/Utils/Peso.cs
+++ Selvagem/Utils/Peso.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace Selvagem.Utils
 {
-    public struct Peso
+    public struct Peso : IEquatable<Peso>, IComparable<Peso>, IComparable
     {
         public decimal Kilos { get; }
 
@@ -12,9 +12,71 @@ namespace Selvagem.Utils
         {
             Kilos = kilos;
         }
+        
 
         public static Peso FromKilos(decimal kilos) => new Peso(kilos);
         public static Peso FromGramas(decimal gramas) => new Peso(gramas / 1000);
         public static Peso FromToneladas(decimal toneladas) => new Peso(toneladas * 1000);
+
+        public override string ToString() => Kilos.ToString() + " Kg";
+        
+        public bool Equals(Peso other)
+        {
+            return Kilos == other.Kilos;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Peso other && Equals(other);
+        }
+
+        public override int GetHashCode()
+        {
+            return Kilos.GetHashCode();
+        }
+
+        public static bool operator ==(Peso left, Peso right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(Peso left, Peso right)
+        {
+            return !left.Equals(right);
+        }
+
+        public int CompareTo(Peso other)
+        {
+            return Kilos.CompareTo(other.Kilos);
+        }
+
+        public int CompareTo(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return 1;
+            return obj is Peso other ? CompareTo(other) : throw new ArgumentException($"Object must be of type {nameof(Peso)}");
+        }
+
+        public static bool operator <(Peso left, Peso right)
+        {
+            return left.CompareTo(right) < 0;
+        }
+
+        public static bool operator >(Peso left, Peso right)
+        {
+            return left.CompareTo(right) > 0;
+        }
+
+        public static bool operator <=(Peso left, Peso right)
+        {
+            return left.CompareTo(right) <= 0;
+        }
+
+        public static bool operator >=(Peso left, Peso right)
+        {
+            return left.CompareTo(right) >= 0;
+        }
+        
+        public static implicit operator decimal(Peso peso) => peso.Kilos;
+        public static explicit operator Peso(decimal pesoEmKilos) => FromKilos(pesoEmKilos);
     }
 }
