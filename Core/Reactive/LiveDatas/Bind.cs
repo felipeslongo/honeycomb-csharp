613593d57f95f908d81843c69c0a36002e5140c3
Felipe de Souza Longo <felipeslongo@gmail.com>
2019-10-13 02:18:32 -0300

    Update Bind.cs

--- Core/Reactive/LiveDatas/Bind.cs
+++ Core/Reactive/LiveDatas/Bind.cs
@@ -1,38 +1,38 @@
-﻿using Core.Threading;
-using System;
-using System.Threading;
+﻿//using Core.Threading;
+//using System;
+//using System.Threading;
 
-namespace Core.Reactive.LiveDatas
-{
-    [Obsolete("Unstable due to high memory pressure", true)]
-    public class Bind : IDisposable
-    {
-        private readonly EventHandler<EventArgs> _handler;
-        private Action _unbind;
-        private SynchronizationContext _context;
+//namespace Core.Reactive.LiveDatas
+//{
+//    [Obsolete("Unstable due to high memory pressure", true)]
+//    public class Bind : IDisposable
+//    {
+//        private readonly EventHandler<EventArgs> _handler;
+//        private Action _unbind;
+//        private SynchronizationContext _context;
 
-        internal Bind(EventHandler<EventArgs> handler)
-        {
-            _handler = handler;
-        }
+//        internal Bind(EventHandler<EventArgs> handler)
+//        {
+//            _handler = handler;
+//        }
 
-        public void WithCurrentSynchronizationContext() => WithSynchronizationContext(SynchronizationContext.Current);
+//        public void WithCurrentSynchronizationContext() => WithSynchronizationContext(SynchronizationContext.Current);
 
-        public void WithSynchronizationContext(SynchronizationContext context) => _context = context;
+//        public void WithSynchronizationContext(SynchronizationContext context) => _context = context;
 
-        internal void ConfigureUnbind(Action unbind) => _unbind = unbind;
+//        internal void ConfigureUnbind(Action unbind) => _unbind = unbind;
 
-        public void Dispose() => _unbind();
+//        public void Dispose() => _unbind();
 
-        internal void OnLiveDataPropertyChanged(object sender, EventArgs e)
-        {
-            if (_context == null)
-            {
-                _handler(sender, e);
-                return;
-            }
+//        internal void OnLiveDataPropertyChanged(object sender, EventArgs e)
+//        {
+//            if (_context == null)
+//            {
+//                _handler(sender, e);
+//                return;
+//            }
 
-            _context.Post(_ => _handler(sender, e), null);
-        }
-    }
-}
+//            _context.Post(_ => _handler(sender, e), null);
+//        }
+//    }
+//}
