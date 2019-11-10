bc1c5fd14fd76dfd48de5a18fed23895cb522669
Felipe de Souza Longo <felipeslongo@gmail.com>
2019-11-10 16:26:52 -0300

    Create TextView+SetTextColorStateList.cs

---
+++ HoneyComb.Platform.Android/Widget/TextView+SetTextColorStateList.cs
@@ -0,0 +1,14 @@
+﻿using Android.Widget;
+using HoneyComb.Platform.Android.Content.Res;
+using System;
+namespace HoneyComb.Platform.Android.Widget
+{
+    public static class TextViewSetTextColorStateList
+    {
+        public static void SetTextColorStateList(this TextView @this, int colorResourceId)
+        {
+            var color = ColorService.GetColorStateList(@this.Context, colorResourceId);
+            @this.SetTextColor(color);
+        }
+    }
+}
