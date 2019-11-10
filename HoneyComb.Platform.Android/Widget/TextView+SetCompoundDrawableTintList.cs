ca3dfe82de6db4f5a2caba8e04c21c82d4168f7e
Felipe de Souza Longo <felipeslongo@gmail.com>
2019-11-10 16:21:20 -0300

    add TextViewSetCompoundDrawableTintList

---
+++ HoneyComb.Platform.Android/Widget/TextView+SetCompoundDrawableTintList.cs
@@ -0,0 +1,11 @@
+﻿using Android.Widget;
+using HoneyComb.Platform.Android.Content.Res;
+
+namespace HoneyComb.Platform.Android.Widget
+{
+    public static class TextViewSetCompoundDrawableTintList
+    {
+        public static void SetCompoundDrawableTintList(this TextView @this, int colorResourceId) =>
+            @this.CompoundDrawableTintList = ColorService.GetColorStateList(@this.Context, colorResourceId);
+    }
+}
