7e35797dbcaee25813ead36b266fe189148414bc
Felipe de Souza Longo <felipeslongo@gmail.com>
2019-11-10 15:49:23 -0300

    create ColorService + GetColorStateList

---
+++ HoneyComb.Platform.Android/UI/Colors/ColorService.cs
@@ -0,0 +1,26 @@
+﻿using Android.Content;
+using Android.Content.Res;
+using Android.Support.V4.Content;
+
+namespace HoneyComb.Platform.Android.UI.Colors
+{
+    /// <summary>
+    /// Provide Color helpers and utils.
+    /// </summary>
+    public static class ColorService
+    {
+        /// <summary>
+        /// Gets an <see cref="ColorStateList"/> from a color resource id.
+        /// </summary>
+        /// <param name="context">Context.</param>
+        /// <param name="colorResourceId">Android color resource id.</param>
+        /// <returns><see cref="ColorStateList"/> instance.</returns>
+        /// <exception cref="System.Exception">
+        /// when <paramref name="context"/> is null.
+        /// when <paramref name="colorResourceId"/> is not found.
+        /// when <paramref name="colorResourceId"/> is not a color resource.
+        /// </exception>
+        public static ColorStateList GetColorStateList(Context context, int colorResourceId) =>
+            ContextCompat.GetColorStateList(context, colorResourceId);
+    }
+}
