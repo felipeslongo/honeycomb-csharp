89886c1aa62741fd2e9f2a461a12be8539d96f1b
Felipe de Souza Longo <felipeslongo@gmail.com>
2019-11-11 00:37:23 -0300

    create LiveDataBindTextViewText

---
+++ HoneyComb.LiveDataNet.Platform.Android/BindingAdapters/LiveData+BindTextViewText.cs
@@ -0,0 +1,22 @@
+﻿using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HoneyComb.LiveDataNet.Platform.Android.BindingAdapters
+{
+    public static class LiveDataBindTextViewText
+    {
+        /// <summary>
+        /// Bind an <see cref="string"/> to an <see cref="TextView.Text"/> property.
+        /// </summary>
+        /// <param name="liveData">An <see cref="LiveData{string}"/>.</param>
+        /// <param name="textView">Target</param>
+        /// <returns>Binding <see cref="IDisposable"/></returns>
+        /// <exception cref="Exception">To be added</exception>
+        public static IDisposable BindTextViewText(this LiveData<string> liveData, TextView textView) =>
+            liveData.Bind(() => textView.Text);
+    }
+}
