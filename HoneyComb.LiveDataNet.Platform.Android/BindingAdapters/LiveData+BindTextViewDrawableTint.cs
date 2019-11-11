cb5bb390e7178b30cb4b83cad549fec68677b033
Felipe de Souza Longo <felipeslongo@gmail.com>
2019-11-11 00:30:30 -0300

    create LiveDataBindTextViewDrawableTint

---
+++ HoneyComb.LiveDataNet.Platform.Android/BindingAdapters/LiveData+BindTextViewDrawableTint.cs
@@ -0,0 +1,18 @@
+﻿using Android.Widget;
+using HoneyComb.Platform.Android.Widget;
+using System;
+
+namespace HoneyComb.LiveDataNet.Platform.Android.BindingAdapters
+{
+    public static class LiveDataBindTextViewDrawableTint
+    {
+        /// <summary>
+        /// Bind a Color to an <see cref="TextView.CompoundDrawableTintList"/> property.
+        /// </summary>
+        /// <param name="liveData">An <see cref="LiveData{Int32}"/> with a color state list resource.</param>
+        /// <param name="textView">Target</param>
+        /// <returns>Binding <see cref="IDisposable"/></returns>
+        public static IDisposable BindTextViewDrawableTint(this LiveData<int> liveData, TextView textView) =>
+            liveData.BindMethod(textView.SetCompoundDrawableTintList);
+    }
+}
