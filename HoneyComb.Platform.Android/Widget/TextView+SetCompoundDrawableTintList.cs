5a240fda0eafecfdf0c80d06fd7addafe608ce92
Felipe de Souza Longo <felipeslongo@gmail.com>
2019-11-11 00:08:43 -0300

    CreateHoneyComb.Platform.Android (#7)
    * create HoneyComb.Platform.Android
    
    * create ColorService + GetColorStateList
    
    * add C# 8 Nullable and TreatWarningsAsErrors
    
    * move ColorService to Content.Res namespace
    
    * add TextViewSetCompoundDrawableTintList
    
    * Create TextView+SetTextColorStateList.cs
    
    * Update HoneyComb.Platform.Android.csproj
    
    * add sumarry

---
+++ HoneyComb.Platform.Android/Widget/TextView+SetCompoundDrawableTintList.cs
@@ -0,0 +1,16 @@
+﻿using Android.Widget;
+using HoneyComb.Platform.Android.Content.Res;
+
+namespace HoneyComb.Platform.Android.Widget
+{
+    public static class TextViewSetCompoundDrawableTintList
+    {
+        /// <summary>
+        /// Sets the left, right, top and bottom drawable tint.
+        /// </summary>
+        /// <param name="this"></param>
+        /// <param name="colorResourceId">Color state list resource</param>
+        public static void SetCompoundDrawableTintList(this TextView @this, int colorResourceId) =>
+            @this.CompoundDrawableTintList = ColorService.GetColorStateList(@this.Context, colorResourceId);
+    }
+}
---
+++ HoneyComb.Platform.Android/Widget/TextView+SetTextColorStateList.cs
@@ -0,0 +1,19 @@
+﻿using Android.Widget;
+using HoneyComb.Platform.Android.Content.Res;
+using System;
+namespace HoneyComb.Platform.Android.Widget
+{
+    public static class TextViewSetTextColorStateList
+    {
+        /// <summary>
+        /// Sets the text color.
+        /// </summary>
+        /// <param name="this">Extended object.</param>
+        /// <param name="colorResourceId">Color state list resource</param>
+        public static void SetTextColorStateList(this TextView @this, int colorResourceId)
+        {
+            var color = ColorService.GetColorStateList(@this.Context, colorResourceId);
+            @this.SetTextColor(color);
+        }
+    }
+}
