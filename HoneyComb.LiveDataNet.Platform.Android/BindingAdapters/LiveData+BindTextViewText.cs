c930c4f36f9f1f9e09479beba56fae0099569d8b
Felipe de Souza Longo <felipeslongo@gmail.com>
2019-11-11 00:41:47 -0300

    create LiveDataBindTextViewTextColor

--- HoneyComb.LiveDataNet.Platform.Android/BindingAdapters/LiveData+BindTextViewText.cs
+++ HoneyComb.LiveDataNet.Platform.Android/BindingAdapters/LiveData+BindTextViewText.cs
@@ -1,9 +1,5 @@
 ﻿using Android.Widget;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HoneyComb.LiveDataNet.Platform.Android.BindingAdapters
 {
