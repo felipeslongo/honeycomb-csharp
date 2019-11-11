2f78d9e8ade45771070c006a7c210fe77a9bb9a5
Felipe de Souza Longo <felipeslongo@gmail.com>
2019-11-11 00:43:23 -0300

    create LiveDataBindViewActivated

---
+++ HoneyComb.LiveDataNet.Platform.Android/BindingAdapters/LiveData+BindViewActivated.cs
@@ -0,0 +1,18 @@
+﻿using Android.Views;
+using System;
+
+namespace HoneyComb.LiveDataNet.Platform.Android.BindingAdapters
+{
+    public static class LiveDataBindViewActivated
+    {
+        /// <summary>
+        /// Bind an <see cref="bool"/> to an <see cref="View.Activated"/> property.
+        /// </summary>
+        /// <param name="liveData">An <see cref="LiveData{bool}"/> representing the Activated state.</param>
+        /// <param name="textView">Target</param>
+        /// <returns>Binding <see cref="IDisposable"/></returns>
+        /// <exception cref="Exception">To be added</exception>
+        public static IDisposable BindViewActivated(this LiveData<bool> liveData, View view) =>
+            liveData.Bind(() => view.Activated);
+    }
+}
