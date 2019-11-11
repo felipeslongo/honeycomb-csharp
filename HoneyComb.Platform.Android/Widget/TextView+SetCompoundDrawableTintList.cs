6ad65735d98c19fe829bb3ed609b6ef4fe8f83cb
Felipe de Souza Longo <felipeslongo@gmail.com>
2019-11-11 00:05:20 -0300

    add sumarry

--- HoneyComb.Platform.Android/Widget/TextView+SetCompoundDrawableTintList.cs
+++ HoneyComb.Platform.Android/Widget/TextView+SetCompoundDrawableTintList.cs
@@ -5,6 +5,11 @@ namespace HoneyComb.Platform.Android.Widget
 {
     public static class TextViewSetCompoundDrawableTintList
     {
+        /// <summary>
+        /// Sets the left, right, top and bottom drawable tint.
+        /// </summary>
+        /// <param name="this"></param>
+        /// <param name="colorResourceId">Color state list resource</param>
         public static void SetCompoundDrawableTintList(this TextView @this, int colorResourceId) =>
             @this.CompoundDrawableTintList = ColorService.GetColorStateList(@this.Context, colorResourceId);
     }
--- HoneyComb.Platform.Android/Widget/TextView+SetTextColorStateList.cs
+++ HoneyComb.Platform.Android/Widget/TextView+SetTextColorStateList.cs
@@ -5,6 +5,11 @@ namespace HoneyComb.Platform.Android.Widget
 {
     public static class TextViewSetTextColorStateList
     {
+        /// <summary>
+        /// Sets the text color.
+        /// </summary>
+        /// <param name="this">Extended object.</param>
+        /// <param name="colorResourceId">Color state list resource</param>
         public static void SetTextColorStateList(this TextView @this, int colorResourceId)
         {
             var color = ColorService.GetColorStateList(@this.Context, colorResourceId);
