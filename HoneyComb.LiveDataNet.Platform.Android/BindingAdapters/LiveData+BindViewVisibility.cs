f3fce01f1129a0af535cf5abaad021a186efdc6e
Felipe de Souza Longo <felipeslongo@gmail.com>
2019-11-20 23:03:42 -0300

    Create LiveData+BindViewVisibility.cs

---
+++ HoneyComb.LiveDataNet.Platform.Android/BindingAdapters/LiveData+BindViewVisibility.cs
@@ -0,0 +1,22 @@
+﻿using Android.Views;
+using System;
+
+namespace HoneyComb.LiveDataNet.Platform.Android.BindingAdapters
+{
+    public static class LiveDataBindViewVisibility
+    {
+        /// <summary>
+        /// Bind an <see cref="bool"/> to an <see cref="View.Visibility"/> property.
+        /// </summary>
+        /// <param name="liveData">An <see cref="LiveData{bool}"/> representing the Visibility state.</param>
+        /// <param name="view">Target</param>
+        /// <param name="falseViewState">State used when <paramref name="liveData"/> is false. Default to <see cref="ViewStates.Gone"/>.</param>
+        /// <returns>Binding <see cref="IDisposable"/></returns>
+        /// <exception cref="Exception">To be added</exception>
+        public static IDisposable BindViewVisibility(this LiveData<bool> liveData, View view, ViewStates falseViewState = ViewStates.Gone) =>
+            liveData.BindMethod(visibility => view.Visibility = ConvertBooleanToViewState(visibility, falseViewState));
+
+        private static ViewStates ConvertBooleanToViewState(bool visibility, ViewStates falseViewState) => 
+            visibility ? ViewStates.Visible : falseViewState;
+    }
+}
