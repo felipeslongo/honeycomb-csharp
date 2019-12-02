92df7fe8de30f12d280157dc9f4f8a86d9b9a133
Felipe de Souza Longo <felipeslongo@gmail.com>
2019-12-02 01:53:10 -0300

    Create LifecycleDisposable.cs

---
+++ HoneyComb.Platform.Android/Lifecycle/LifecycleDisposable.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HoneyComb.Platform.Android.Lifecycle
+{
+    /// <summary>
+    /// Lifecycle-aware composite disposable collection.
+    /// It will dispose all itens when an Lifecycle component
+    /// is destroyed.
+    ///
+    /// Use it to avoid memory leaks in your application.
+    /// </summary>
+    public class LifecycleDisposable : IDisposable
+    {
+        private readonly List<Action> disposables = new List<Action>();
+
+        internal LifecycleDisposable(LifecycleObservable observable)
+        {
+            disposables.Add(() => observable.OnDestroy -= LifecycleObservableOnDestroy);
+            observable.OnDestroy += LifecycleObservableOnDestroy;
+        }
+
+        public bool IsDisposed { get; private set; }
+
+        public void Add(Action disposable)
+        {
+            GuardIsDisposed();
+            disposables.Add(disposable);
+        }
+
+        public void Add(IDisposable disposable) => Add(disposable.Dispose);
+
+        public void Dispose()
+        {
+            GuardIsDisposed();
+
+            disposables.ForEach(disposable => disposable());
+            disposables.Clear();
+            IsDisposed = true;
+        }
+
+        private void GuardIsDisposed()
+        {
+            if (IsDisposed)
+                throw new ObjectDisposedException(nameof(LifecycleDisposable));
+        }
+
+        private void LifecycleObservableOnDestroy(object sender, EventArgs e) => Dispose();
+    }
+}
