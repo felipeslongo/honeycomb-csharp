9b3aacbe1d49c033dba79716fe4c25350a568ae3
Felipe de Souza Longo <felipeslongo@gmail.com>
2019-12-02 01:11:53 -0300

    Create LifecycleObservable

---
+++ HoneyComb.Platform.Android/Lifecycle/LifecycleObservable.cs
@@ -0,0 +1,73 @@
+﻿using Android.Arch.Lifecycle;
+using Java.Interop;
+using System;
+using JavaObject = Java.Lang.Object;
+using LifecycleAndroid = Android.Arch.Lifecycle.Lifecycle;
+
+namespace HoneyComb.Platform.Android.Lifecycle
+{
+    /// <summary>
+    /// Android Lifecycle-aware observer.
+    /// Use it as a Observable object to execute
+    /// code that is lifecycle dependent.
+    /// </summary>
+    public class LifecycleObservable : JavaObject, ILifecycleObserver
+    {
+        public EventHandler? OnAny;
+        public EventHandler? OnCreate;
+        public EventHandler? OnDestroy;
+        public EventHandler? OnPause;
+        public EventHandler? OnResume;
+        public EventHandler? OnStart;
+        public EventHandler? OnStop;
+        private ILifecycleOwner? _lifecycleOwner;
+
+        public void ObserveLifecycleOwner(ILifecycleOwner lifecycleOwner)
+        {
+            _lifecycleOwner = lifecycleOwner;
+            _lifecycleOwner.Lifecycle.AddObserver(this);
+        }
+
+        [LifecycleAndroid.Event.OnAny, Export]
+        public void OnLifecycleEventOnAny()
+        {
+            OnAny?.Invoke(this, EventArgs.Empty);
+        }
+
+        [LifecycleAndroid.Event.OnCreate, Export]
+        public void OnLifecycleEventOnCreate()
+        {
+            OnCreate?.Invoke(this, EventArgs.Empty);
+        }
+
+        [LifecycleAndroid.Event.OnDestroy, Export]
+        public void OnLifecycleEventOnDestroy()
+        {
+            OnDestroy?.Invoke(this, EventArgs.Empty);
+        }
+
+        [LifecycleAndroid.Event.OnPause, Export]
+        public void OnLifecycleEventOnPause()
+        {
+            OnPause?.Invoke(this, EventArgs.Empty);
+        }
+
+        [LifecycleAndroid.Event.OnResume, Export]
+        public void OnLifecycleEventOnResume()
+        {
+            OnResume?.Invoke(this, EventArgs.Empty);
+        }
+
+        [LifecycleAndroid.Event.OnStart, Export]
+        public void OnLifecycleEventOnStart()
+        {
+            OnStart?.Invoke(this, EventArgs.Empty);
+        }
+
+        [LifecycleAndroid.Event.OnStop, Export]
+        public void OnLifecycleEventOnStop()
+        {
+            OnStop?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
---
+++ HoneyComb.Platform.Android/Lifecycle/LifecycleService.cs
@@ -0,0 +1,17 @@
+﻿using Android.Arch.Lifecycle;
+
+namespace HoneyComb.Platform.Android.Lifecycle
+{
+    /// <summary>
+    /// Provides utilities and helpers related 
+    /// to Android Lifecycle Components.
+    /// </summary>
+    public static class LifecycleService
+    {
+        public static LifecycleObservable GetObservable(ILifecycleOwner lifecycleOwner)
+        {
+            var observable = new LifecycleObservable();
+            return observable;
+        }
+    }
+}
