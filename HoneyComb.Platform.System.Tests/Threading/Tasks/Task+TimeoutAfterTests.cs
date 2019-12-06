d441144fbff220f80e56935afd6870252e7a5b13
Felipe de Souza Longo <felipeslongo@gmail.com>
2019-12-06 15:39:44 -0300

    Create Task.FromTimeout (#8)
    * update /.ideia/* by rider in mac
    
    * Create project HoneyComb.Platform.System.csproj
    
    * Create Task.TimeoutAfter extension method
    
    Signed-off-by: Felipe de Souza Longo <felipeslongo@gmail.com>
    
    * Create HoneyComb.Platform.System.Tests.csproj project
    
    Signed-off-by: Felipe de Souza Longo <felipeslongo@gmail.com>
    
    * Create Task+TimeoutAfterTests.cs tests
    
    Signed-off-by: Felipe de Souza Longo <felipeslongo@gmail.com>

---
+++ HoneyComb.Platform.System.Tests/Threading/Tasks/Task+TimeoutAfterTests.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using HoneyComb.Platform.System.Threading.Tasks;
+using HoneyComb.TestChamber;
+using Xunit;
+
+namespace HoneyComb.Platform.System.Tests.Threading.Tasks
+{
+    public class TaskTimeoutAfterTests
+    {
+        [Fact]
+        [Trait(nameof(Category), Category.Unit)]
+        public async Task GivenAnCompletedTask_ShouldReturnImmediatelly()
+        {
+            var completedTask = Task.CompletedTask;
+            var timeout = TimeSpan.FromSeconds(1);
+
+            await completedTask.TimeoutAfter(timeout);
+        }
+
+        [Fact]
+        [Trait(nameof(Category), Category.Unit)]
+        public async Task GivenAnFasterThanTimeoutTask_WhenTaskIsFinished_ShouldReturn()
+        {
+            var shortRunningTask = Task.Delay(TimeSpan.FromMilliseconds(10));
+            var timeout = TimeSpan.FromMilliseconds(20);
+
+            await shortRunningTask.TimeoutAfter(timeout);
+        }
+
+        [Fact]
+        [Trait(nameof(Category), Category.Unit)]
+        public async Task GivenAnInfiniteTimeout_ShouldWaitForeverTheTaskReturn()
+        {
+            var task = Task.Delay(TimeSpan.FromMilliseconds(10));
+            var infiniteTimeout = Timeout.InfiniteTimeSpan;
+
+            await task.TimeoutAfter(infiniteTimeout);
+        }
+
+        [Fact]
+        [Trait(nameof(Category), Category.Unit)]
+        public async Task GivenAnSlowerThanTimeoutTask_WhenTimeoutIsReached_ShouldThrowAException()
+        {
+            var longRunningTask = Task.Delay(TimeSpan.FromMilliseconds(20));
+            var timeout = TimeSpan.FromMilliseconds(10);
+
+            async Task TestCode()
+            {
+                await longRunningTask.TimeoutAfter(timeout);
+            }
+
+            await Assert.ThrowsAsync<TimeoutException>(TestCode);
+        }
+
+        [Fact]
+        [Trait(nameof(Category), Category.Unit)]
+        public async Task GivenAnValueReturningTask_WhenTaskIsFinished_ShouldReturnTheValue()
+        {
+            var valueExpected = new object();
+            var valueTask = Task.Run(() => valueExpected);
+            var timeout = TimeSpan.FromMilliseconds(20);
+
+            var actual = await valueTask.TimeoutAfter(timeout);
+
+            Assert.Equal(valueExpected, actual);
+        }
+
+        [Fact]
+        [Trait(nameof(Category), Category.Unit)]
+        public async Task GivenAnZeroTimeout_ShouldThrowAExceptionImmediately()
+        {
+            var task = Task.Delay(TimeSpan.FromMilliseconds(10));
+            var zeroTimeout = TimeSpan.Zero;
+
+            await Assert.ThrowsAsync<TimeoutException>(() => task.TimeoutAfter(zeroTimeout));
+        }
+    }
+}
---
+++ HoneyComb.Platform.System/Threading/Tasks/Task+TimeoutAfter.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HoneyComb.Platform.System.Threading.Tasks
+{
+    /// <summary>
+    ///     Static class for the extension method TimeoutAfter in Task.
+    /// </summary>
+    /// <remarks>
+    ///     True authors of this feature:
+    ///     https://stackoverflow.com/questions/4238345/asynchronously-wait-for-taskt-to-complete-with-timeout"
+    ///     https://devblogs.microsoft.com/pfxteam/crafting-a-task-timeoutafter-method/
+    /// </remarks>
+    public static class TaskTimeoutAfter
+    {
+        /// <summary>
+        ///     Obtain a copy or “proxy” of the Task that will either
+        ///     (A) complete within the specified time period, or
+        ///     (B) will complete with an indication that it had timed out.
+        /// </summary>
+        /// <param name="task">Task that will be awaited</param>
+        /// <param name="timeout">Timeout that should be awaited till the <paramref name="task" /> completes</param>
+        /// <typeparam name="TResult">Generic result returned by <paramref name="task" /></typeparam>
+        /// <returns>
+        ///     The <typeparamref name="TResult" /> or
+        ///     a <exception cref="TimeoutException"></exception>
+        /// </returns>
+        /// <exception cref="TimeoutException">If the timeout is reached before the task completes</exception>
+        /// <remarks>
+        ///     The returned proxy Task can complete in one of two ways:
+        ///     If task completes before the specified timeout period has elapsed,
+        ///     then the proxy Task finishes when task finishes,
+        ///     with task’s completion status being copied to the proxy.
+        ///     If task fails to complete before the specified timeout period has elapsed,
+        ///     then the proxy Task finishes when the timeout period expires,
+        ///     in Faulted state with a TimeoutException.
+        /// </remarks>
+        public static async Task<TResult> TimeoutAfter<TResult>(this Task<TResult> task, TimeSpan timeout)
+        {
+            await TimeoutAfter(task as Task, timeout);
+            return await task; // Very important in order to propagate exceptions
+
+            // using var timeoutCancellationTokenSource = new CancellationTokenSource();
+            // var timeoutTask = Task.Delay(timeout, timeoutCancellationTokenSource.Token);
+            // var completedTask = await Task.WhenAny(task, timeoutTask);
+            // if (completedTask == timeoutTask) throw new TimeoutException();
+            //
+            // timeoutCancellationTokenSource.Cancel();
+            // return await task;  // Very important in order to propagate exceptions
+        }
+
+        public static async Task TimeoutAfter(this Task task, TimeSpan timeout)
+        {
+            if (ShouldShortCircuit(task, timeout)) return;
+
+
+            using var timeoutCancellationTokenSource = new CancellationTokenSource();
+            var timeoutTask = Task.Delay(timeout, timeoutCancellationTokenSource.Token);
+            var completedTask = await Task.WhenAny(task, timeoutTask);
+            if (completedTask == timeoutTask) throw new TimeoutException();
+
+            timeoutCancellationTokenSource.Cancel();
+            await task; // Very important in order to propagate exceptions
+        }
+
+        private static bool ShouldShortCircuit(Task task, TimeSpan timeout)
+        {
+            // Short-circuit #1: infinite timeout or task already completed
+            if (task.IsCompleted || timeout == Timeout.InfiniteTimeSpan)
+                // Either the task has already completed or timeout will never occur.
+                // No proxy necessary.
+                return true;
+
+            // Short-circuit #2: zero timeout
+            if (timeout == TimeSpan.Zero)
+                throw new TimeoutException(); // We've already timed out.
+
+            return false;
+        }
+    }
+}
