03cb646179e8f5bc8d71b94bd259e8912f3d1530
Felipe de Souza Longo <felipeslongo@gmail.com>
2019-12-07 16:01:39 -0300

    Modify LifecycleObservable.cs add event keyword
    Signed-off-by: Felipe de Souza Longo <felipeslongo@gmail.com>

--- HoneyComb.Platform.Android/Lifecycle/LifecycleObservable.cs
+++ HoneyComb.Platform.Android/Lifecycle/LifecycleObservable.cs
@@ -1,32 +1,25 @@
-﻿using Android.Arch.Lifecycle;
+﻿using System;
+using Android.Arch.Lifecycle;
 using Java.Interop;
-using System;
 using static Android.Arch.Lifecycle.Lifecycle;
 using JavaObject = Java.Lang.Object;
-
 using LifecycleAndroid = Android.Arch.Lifecycle.Lifecycle;
 
 namespace HoneyComb.Platform.Android.Lifecycle
 {
     /// <summary>
-    /// Android Lifecycle-aware observer.
-    /// Use it as a Observable object to execute
-    /// code that is lifecycle dependent.
+    ///     Android Lifecycle-aware observer.
+    ///     Use it as a Observable object to execute
+    ///     code that is lifecycle dependent.
     /// </summary>
     /// <remarks>
     ///     Inspiration
-    ///     - LifecycleBoundObserver: https://android.googlesource.com/platform/frameworks/support/+/androidx-master-dev/lifecycle/lifecycle-livedata-core/src/main/java/androidx/lifecycle/LiveData.java
+    ///     - LifecycleBoundObserver:
+    ///     https://android.googlesource.com/platform/frameworks/support/+/androidx-master-dev/lifecycle/lifecycle-livedata-core/src/main/java/androidx/lifecycle/LiveData.java
     /// </remarks>
     public class LifecycleObservable : JavaObject, ILifecycleObserver
     {
-        public EventHandler? OnAny;
-        public EventHandler? OnCreate;
-        public EventHandler? OnDestroy;
-        public EventHandler? OnPause;
-        public EventHandler? OnResume;
-        public EventHandler? OnStart;
-        public EventHandler? OnStop;
-        private ILifecycleOwner? _lifecycleOwner;
+        private readonly ILifecycleOwner? _lifecycleOwner;
 
         internal LifecycleObservable(ILifecycleOwner lifecycleOwner)
         {
@@ -36,53 +29,69 @@ namespace HoneyComb.Platform.Android.Lifecycle
         }
 
         /// <summary>
-        /// Gets the Lifecycle <see cref="LifecycleAndroid.CurrentState"/> current state.
-        /// Return null when this observer is removed from the <see cref="LifecycleAndroid"/> or disposed.
+        ///     Gets the Lifecycle <see cref="LifecycleAndroid.CurrentState" /> current state.
+        ///     Return null when this observer is removed from the <see cref="LifecycleAndroid" /> or disposed.
         /// </summary>
         public State? StateCurrent => _lifecycleOwner?.Lifecycle.CurrentState;
+
         /// <summary>
-        /// Gets the last <see cref="StateCurrent"/> captured.
+        ///     Gets the last <see cref="StateCurrent" /> captured.
         /// </summary>
         public State StateLastKnown { get; private set; }
 
-        [LifecycleAndroid.Event.OnAny, Export]
+        public event EventHandler? OnAny;
+        public event EventHandler? OnCreate;
+        public event EventHandler? OnDestroy;
+        public event EventHandler? OnPause;
+        public event EventHandler? OnResume;
+        public event EventHandler? OnStart;
+        public event EventHandler? OnStop;
+
+        [Event.OnAnyAttribute]
+        [Export]
         public void OnLifecycleEventOnAny()
         {
             StateLastKnown = StateCurrent!;
             OnAny?.Invoke(this, EventArgs.Empty);
         }
 
-        [LifecycleAndroid.Event.OnCreate, Export]
+        [Event.OnCreateAttribute]
+        [Export]
         public void OnLifecycleEventOnCreate()
         {
             OnCreate?.Invoke(this, EventArgs.Empty);
         }
 
-        [LifecycleAndroid.Event.OnDestroy, Export]
+        [Event.OnDestroyAttribute]
+        [Export]
         public void OnLifecycleEventOnDestroy()
         {
             OnDestroy?.Invoke(this, EventArgs.Empty);
         }
 
-        [LifecycleAndroid.Event.OnPause, Export]
+        [Event.OnPauseAttribute]
+        [Export]
         public void OnLifecycleEventOnPause()
         {
             OnPause?.Invoke(this, EventArgs.Empty);
         }
 
-        [LifecycleAndroid.Event.OnResume, Export]
+        [Event.OnResumeAttribute]
+        [Export]
         public void OnLifecycleEventOnResume()
         {
             OnResume?.Invoke(this, EventArgs.Empty);
         }
 
-        [LifecycleAndroid.Event.OnStart, Export]
+        [Event.OnStartAttribute]
+        [Export]
         public void OnLifecycleEventOnStart()
         {
             OnStart?.Invoke(this, EventArgs.Empty);
         }
 
-        [LifecycleAndroid.Event.OnStop, Export]
+        [Event.OnStopAttribute]
+        [Export]
         public void OnLifecycleEventOnStop()
         {
             OnStop?.Invoke(this, EventArgs.Empty);
