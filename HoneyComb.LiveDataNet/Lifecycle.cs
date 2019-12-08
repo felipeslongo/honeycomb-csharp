abc31e0df6381746a809689d3b7c0f0bedbe2156
Felipe de Souza Longo <felipeslongo@gmail.com>
2019-12-08 10:19:31 -0300

    Modify Lifecycle add NotifyObserversOf*

--- HoneyComb.LiveDataNet/Lifecycle.cs
+++ HoneyComb.LiveDataNet/Lifecycle.cs
@@ -11,14 +11,43 @@ namespace HoneyComb.LiveDataNet
     /// Credits:
     ///     https://developer.android.com/reference/androidx/lifecycle/Lifecycle.html
     /// </remarks>
-    public abstract class Lifecycle
+    public abstract class Lifecycle : IDisposable
     {
-        public List<ILifecycleObserver> _observers = new List<ILifecycleObserver>();
+        private readonly List<ILifecycleObserver> observers = new List<ILifecycleObserver>();
+        private ILifecycleOwner? owner;
+
+        protected Lifecycle(ILifecycleOwner owner)
+        {
+            this.owner = owner;
+        }
+
+        public virtual void Dispose()
+        {
+            owner = null;
+            ClearObservers();
+        }
 
         public IDisposable Subscribe(ILifecycleObserver lifecycleObserver)
         {
-            _observers.Add(lifecycleObserver);
-            return Disposable.Create(() => _observers.Remove(lifecycleObserver));
+            observers.Add(lifecycleObserver);
+            return Disposable.Create(() => observers.Remove(lifecycleObserver));
+        }
+
+        protected void ClearObservers() => observers.Clear();
+
+        protected void NotifyObserversOfActive()
+        {
+            observers.ForEach(observer => observer.OnActive(owner!));
+        }
+
+        protected void NotifyObserversOfDisposed()
+        {
+            observers.ForEach(observer => observer.OnDisposed(owner!));
+        }
+
+        protected void NotifyObserversOfInactive()
+        {
+            observers.ForEach(observer => observer.OnInactive(owner!));
         }
     }
 }
