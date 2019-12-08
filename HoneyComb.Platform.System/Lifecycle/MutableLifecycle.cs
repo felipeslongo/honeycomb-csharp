4516b4e501a2bb962f9625cd484a83694135e8cc
Felipe de Souza Longo <felipeslongo@gmail.com>
2019-12-08 17:24:09 -0300

    Create MutableLifecycle

---
+++ HoneyComb.Platform.System/Lifecycle/MutableLifecycle.cs
@@ -0,0 +1,14 @@
+﻿namespace HoneyComb.Platform.System.Lifecycle
+{
+    /// <summary>
+    /// Lifecycle that exposes its State as mutable.
+    /// </summary>
+    public sealed class MutableLifecycle : Lifecycle
+    {
+        public MutableLifecycle(ILifecycleOwner owner) : base(owner)
+        {
+        }
+
+        public new void NotifyStateChange(LifecycleState state) => base.NotifyStateChange(state);
+    }
+}
