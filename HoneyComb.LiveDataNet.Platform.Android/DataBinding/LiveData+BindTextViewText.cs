0f80279bb1b485b03c444a64123e489019835f2e
Felipe de Souza Longo <felipeslongo@gmail.com>
2019-12-09 20:35:34 -0300

    Modify LiveData+Bind*.cs add LifecycleOwner support
    Signed-off-by: Felipe de Souza Longo <felipeslongo@gmail.com>

--- HoneyComb.LiveDataNet.Platform.Android/DataBinding/LiveData+BindTextViewText.cs
+++ HoneyComb.LiveDataNet.Platform.Android/DataBinding/LiveData+BindTextViewText.cs
@@ -1,18 +1,23 @@
-﻿using Android.Widget;
-using System;
+﻿using System;
+using Android.Widget;
+using HoneyComb.Core.Lifecycle;
 
 namespace HoneyComb.LiveDataNet.Platform.Android.DataBinding
 {
     public static class LiveDataBindTextViewText
     {
         /// <summary>
-        /// Bind an <see cref="string"/> to an <see cref="TextView.Text"/> property.
+        ///     Bind an <see cref="string" /> to an <see cref="TextView.Text" /> property.
         /// </summary>
-        /// <param name="liveData">An <see cref="LiveData{string}"/>.</param>
+        /// <param name="liveData">An <see cref="LiveData{string}" />.</param>
+        /// <param name="lifecycleOwner">HoneyComb LifecycleOwner</param>
         /// <param name="textView">Target</param>
-        /// <returns>Binding <see cref="IDisposable"/></returns>
+        /// <returns>Binding <see cref="IDisposable" /></returns>
         /// <exception cref="Exception">To be added</exception>
-        public static IDisposable BindTextViewText(this LiveData<string> liveData, TextView textView) =>
-            liveData.Bind(() => textView.Text);
+        public static IDisposable BindTextViewText(this LiveData<string> liveData, ILifecycleOwner lifecycleOwner,
+            TextView textView)
+        {
+            return liveData.Bind(lifecycleOwner, () => textView.Text);
+        }
     }
 }
--- HoneyComb.LiveDataNet.Platform.Android/DataBinding/LiveData+BindViewActivated.cs
+++ HoneyComb.LiveDataNet.Platform.Android/DataBinding/LiveData+BindViewActivated.cs
@@ -1,18 +1,35 @@
-﻿using Android.Views;
-using System;
+﻿using System;
+using Android.Views;
+using HoneyComb.Core.Lifecycle;
 
 namespace HoneyComb.LiveDataNet.Platform.Android.DataBinding
 {
     public static class LiveDataBindViewActivated
     {
         /// <summary>
-        /// Bind an <see cref="bool"/> to an <see cref="View.Activated"/> property.
+        ///     Bind an <see cref="bool" /> to an <see cref="View.Activated" /> property.
         /// </summary>
-        /// <param name="liveData">An <see cref="LiveData{bool}"/> representing the Activated state.</param>
+        /// <param name="liveData">An <see cref="LiveData{bool}" /> representing the Activated state.</param>
         /// <param name="textView">Target</param>
-        /// <returns>Binding <see cref="IDisposable"/></returns>
+        /// <returns>Binding <see cref="IDisposable" /></returns>
         /// <exception cref="Exception">To be added</exception>
-        public static IDisposable BindViewActivated(this LiveData<bool> liveData, View view) =>
-            liveData.Bind(() => view.Activated);
+        public static IDisposable BindViewActivated(this LiveData<bool> liveData, View view)
+        {
+            return liveData.Bind(() => view.Activated);
+        }
+
+        /// <summary>
+        ///     Bind an <see cref="bool" /> to an <see cref="View.Activated" /> property.
+        /// </summary>
+        /// <param name="liveData">An <see cref="LiveData{bool}" /> representing the Activated state.</param>
+        /// <param name="textView">Target</param>
+        /// <param name="lifecycleOwner">HoneyComb LifecycleOwner</param>
+        /// <returns>Binding <see cref="IDisposable" /></returns>
+        /// <exception cref="Exception">To be added</exception>
+        public static IDisposable BindViewActivated(this LiveData<bool> liveData, ILifecycleOwner lifecycleOwner,
+            View view)
+        {
+            return liveData.Bind(lifecycleOwner, () => view.Activated);
+        }
     }
 }
