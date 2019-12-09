3c4dc64d6e9bcaa5d331e6ba5871b58fae00d158
Felipe de Souza Longo <felipeslongo@gmail.com>
2019-12-09 01:52:24 -0300

    Create LifecycleOwners

---
+++ HoneyComb.Platform.Android/Lifecycle/LifecycleOwners.cs
@@ -0,0 +1,23 @@
+﻿using Android.Arch.Lifecycle;
+
+namespace HoneyComb.Platform.Android.Lifecycle
+{
+    /// <summary>
+    /// Companion object that encapsulates
+    /// both LifecyclerOwners from Android
+    /// and from Honeycomb
+    /// </summary>
+    public sealed class LifecycleOwners
+    {
+        public LifecycleOwners(ILifecycleOwner lifecycleOwner)
+        {
+            Android = lifecycleOwner;
+            Observable = LifecycleService.GetObservable(lifecycleOwner);
+            HoneyComb = LifecycleService.GetHoneyCombLifecycleOwner(Observable);
+        }
+
+        public ILifecycleOwner Android { get; }
+        public Core.Lifecycle.ILifecycleOwner HoneyComb { get; }
+        public LifecycleObservable Observable { get; }
+    }
+}
