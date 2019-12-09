37861c1f53bf953dfec4787358213d6887073905
Felipe de Souza Longo <felipeslongo@gmail.com>
2019-12-09 20:18:19 -0300

    Modify LifecycleOwners.cs add LifecycleDisposable
    Signed-off-by: Felipe de Souza Longo <felipeslongo@gmail.com>

--- HoneyComb.Platform.Android/Lifecycle/LifecycleOwners.cs
+++ HoneyComb.Platform.Android/Lifecycle/LifecycleOwners.cs
@@ -3,9 +3,9 @@
 namespace HoneyComb.Platform.Android.Lifecycle
 {
     /// <summary>
-    /// Companion object that encapsulates
-    /// both LifecyclerOwners from Android
-    /// and from Honeycomb
+    ///     Companion object that encapsulates
+    ///     both LifecyclerOwners from Android
+    ///     and from Honeycomb
     /// </summary>
     public sealed class LifecycleOwners
     {
@@ -13,10 +13,12 @@ namespace HoneyComb.Platform.Android.Lifecycle
         {
             Android = lifecycleOwner;
             Observable = LifecycleService.GetObservable(lifecycleOwner);
+            Disposable = LifecycleService.GetDisposable(Observable);
             HoneyComb = LifecycleService.GetHoneyCombLifecycleOwner(Observable);
         }
 
         public ILifecycleOwner Android { get; }
+        public LifecycleDisposable Disposable { get; }
         public Core.Lifecycle.ILifecycleOwner HoneyComb { get; }
         public LifecycleObservable Observable { get; }
     }
