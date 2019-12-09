282a4468235784b04d172501bcc2d3be3fc0abc8
Felipe de Souza Longo <felipeslongo@gmail.com>
2019-12-09 01:52:32 -0300

    Create ActivityCompanion.cs

---
+++ HoneyComb.Platform.Android/AppCompat/App/ActivityCompanion.cs
@@ -0,0 +1,19 @@
+﻿using Android.Support.V7.App;
+using HoneyComb.Platform.Android.Lifecycle;
+
+namespace HoneyComb.Platform.Android.AppCompat.App
+{
+    /// <summary>
+    /// Companion object that provides some
+    /// utilities to an Activity
+    /// </summary>
+    public sealed class ActivityCompanion
+    {
+        public ActivityCompanion(AppCompatActivity activity)
+        {
+            LifecycleOwners = new LifecycleOwners(activity);
+        }
+
+        public LifecycleOwners LifecycleOwners { get; }
+    }
+}
