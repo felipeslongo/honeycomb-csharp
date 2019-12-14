f4e1330723ca4fb6faae8113e27cc4cc9fd8b525
Felipe de Souza Longo <felipeslongo@gmail.com>
2019-12-14 18:26:42 -0300

    Create Event

---
+++ HoneyComb.Platform.System.Tests/EventTests.cs
@@ -0,0 +1,126 @@
+﻿using Core.Infraestrutura.UI;
+using HoneyComb.TestChamber;
+using Xunit;
+
+namespace HoneyComb.Core.Tests
+{
+    public class EventTests
+    {
+        public class ExecuteIfUnhandled : EventTests
+        {
+            [Fact, Trait(nameof(Category), Category.Unit)]
+            public void GivenAHandledEvent_WhenInvoked_ShouldNotExecuteAction()
+            {
+                var wasExecuted = false;
+                var Event = new Event();
+                Event.ExecuteIfUnhandled(() => { });
+
+                Event.ExecuteIfUnhandled(() => wasExecuted = true);
+
+                Assert.False(wasExecuted);
+            }
+
+            [Fact, Trait(nameof(Category), Category.Unit)]
+            public void GivenAnUnhandledEvent_WhenInvoked_ShouldExecuteAction()
+            {
+                var wasExecuted = false;
+                var Event = new Event();
+
+                Event.ExecuteIfUnhandled(() => wasExecuted = true);
+
+                Assert.True(wasExecuted);
+            }
+        }
+
+        public class IsHandledTests : EventTests
+        {
+            [Fact, Trait(nameof(Category), Category.Unit)]
+            public void GivenAHandledEvent_WhenInvoked_ShouldBeTrue()
+            {
+                var Event = new Event();
+                Event.ExecuteIfUnhandled(() => { });
+
+                var @return = Event.IsHandled;
+
+                Assert.True(@return);
+            }
+
+            [Fact, Trait(nameof(Category), Category.Unit)]
+            public void GivenAnUnhandledEvent_WhenInvoked_ShouldBeFalse()
+            {
+                var Event = new Event();
+
+                var @return = Event.IsHandled;
+
+                Assert.False(@return);
+            }
+        }
+    }
+
+    public class EventTests_Generic : EventTests
+    {
+        public class ExecuteIfUnhandledTests_Generic : EventTests_Generic
+        {
+            [Fact, Trait(nameof(Category), Category.Unit)]
+            public void GivenAHandledEvent_WhenInvoked_ShouldNotExecuteAction()
+            {
+                var wasExecuted = false;
+                var Event = new Event<bool>(true);
+                Event.ExecuteIfUnhandled(content => { });
+
+                Event.ExecuteIfUnhandled(content => wasExecuted = content);
+
+                Assert.False(wasExecuted);
+            }
+
+            [Fact, Trait(nameof(Category), Category.Unit)]
+            public void GivenAnUnhandledEvent_WhenInvoked_ShouldExecuteAction()
+            {
+                var wasExecuted = false;
+                var Event = new Event<bool>(true);
+
+                Event.ExecuteIfUnhandled(content => wasExecuted = content);
+
+                Assert.True(wasExecuted);
+            }
+
+            [Fact, Trait(nameof(Category), Category.Unit)]
+            public void GivenAnUnhandledEvent_WhenInvoked_ShouldPassContentAsParameterOfTheAction()
+            {
+                const int expectedArgument = int.MaxValue;
+                var actualArgument = 0;
+                var Event = new Event<int>(expectedArgument);
+
+                Event.ExecuteIfUnhandled(content => actualArgument = content);
+
+                Assert.Equal(expectedArgument, actualArgument);
+            }
+        }
+
+        public class PeekContentTests : EventTests_Generic
+        {
+            [Fact, Trait(nameof(Category), Category.Unit)]
+            public void GivenAHandledEvent_WhenInvoked_ShouldReturnTheContent()
+            {
+                const int expectedContent = int.MaxValue;
+                var Event = new Event<int>(expectedContent);
+                Event.ExecuteIfUnhandled(content => { });
+
+                var actualContent = Event.PeekContent;
+
+                Assert.Equal(expectedContent, actualContent);
+            }
+
+            [Fact, Trait(nameof(Category), Category.Unit)]
+            public void GivenAnUnhandledEvent_WhenInvoked_ShouldReturnTheContent()
+            {
+                const int expectedContent = int.MaxValue;
+                var Event = new Event<int>(expectedContent);
+
+                var actualContent = Event.PeekContent;
+
+                Assert.Equal(expectedContent, actualContent);
+            }
+        }
+    }
+}
---
+++ HoneyComb.Platform.System/Event.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace Core.Infraestrutura.UI
+{
+    /// <summary>
+    /// Represents a regular Event
+    /// that should be consumed only once,
+    /// like an Android Snackbar message,
+    /// a navigation event or a dialog trigger.
+    /// </summary>
+    /// <remarks>
+    /// Credits:
+    ///     https://medium.com/androiddevelopers/livedata-with-snackbar-navigation-and-other-events-the-singleliveevent-case-ac2622673150
+    /// </remarks>
+    public class Event : EventArgs
+    {
+        /// <summary>
+        /// Returns an indicating if the event has been handled.
+        /// </summary>
+        public bool IsHandled { get; private set; }
+
+        /// <summary>
+        /// Executes an action that consumes
+        /// the event and prevents its use again.
+        /// </summary>
+        /// <param name="action">Action to be executed on the vent.</param>
+        public void ExecuteIfUnhandled(Action action)
+        {
+            if (IsHandled)
+                return;
+
+            action();
+            IsHandled = true;
+        }
+    }
+
+    /// <summary>
+    /// Represents a regular Event
+    /// that should be consumed only once,
+    /// like an Android Snackbar message,
+    /// a navigation event or a dialog trigger.
+    /// </summary>
+    /// <typeparam name="T">Event content/args to be handled.</typeparam>
+    public class Event<T> : Event
+    {
+        /// <summary>
+        /// Event content/args.
+        /// </summary>
+        private readonly T content;
+
+        /// <summary>
+        /// Construct an event with content.
+        /// </summary>
+        /// <param name="content">Content to be handled.</param>
+        public Event(T content)
+        {
+            this.content = content;
+        }
+
+        /// <summary>
+        /// Returns the content, even if it's already been handled.
+        /// </summary>
+        public T PeekContent => content;
+
+        /// <summary>
+        /// Executes an action that consumes the event
+        /// content and prevents its use again.
+        /// </summary>
+        /// <param name="action">Action that consumes the event content.</param>
+        public void ExecuteIfUnhandled(Action<T> action) =>
+            base.ExecuteIfUnhandled(() => { action(content); });
+    }
+}
