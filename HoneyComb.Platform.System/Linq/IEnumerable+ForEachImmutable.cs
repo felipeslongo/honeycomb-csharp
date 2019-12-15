c6ca3b4e859d45f6475f4b7d493236d0ba6b96e8
Felipe de Souza Longo <felipeslongo@gmail.com>
2019-12-15 18:44:22 -0300

    Create IEnumerable+ForEachImmutable.cs

---
+++ HoneyComb.Platform.System/Linq/IEnumerable+ForEachImmutable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HoneyComb.Core.Linq
+{
+    public static class IEnumerableForEachImmutable
+    {
+        /// <summary>
+        /// Performs the specified action on each element of the <see cref="IEnumerable{T}"/>.
+        /// Use it if: An element in the collection has been modified should not throw Exception.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="this"></param>
+        /// <param name="action">The System.Action`1 delegate to perform on each element of the System.Collections.Generic.List`1.</param>
+        public static void ForeachImmutable<T>(this IEnumerable<T> @this, Action<T> action) =>
+            @this.ToList().ForEach(action);
+    }
+}
