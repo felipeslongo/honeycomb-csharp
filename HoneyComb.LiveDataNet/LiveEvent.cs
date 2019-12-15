62b4acc86cb56cafb1d30ef54adb22df0af8a173
Felipe de Souza Longo <felipeslongo@gmail.com>
2019-12-15 09:31:14 -0300

    Create LiveEvent.

--- HoneyComb.LiveDataNet/LiveEvent.cs
+++ HoneyComb.LiveDataNet/LiveEvent.cs
@@ -1,7 +1,6 @@
-﻿using HoneyComb.Core.Lifecycles;
+﻿using HoneyComb.Core;
+using HoneyComb.Core.Lifecycles;
 using System;
-using System.Collections.Generic;
-using System.Reactive.Disposables;
 
 namespace HoneyComb.LiveDataNet
 {
@@ -10,25 +9,71 @@ namespace HoneyComb.LiveDataNet
     /// Similar to <see cref="LiveData{T}"/>
     /// but dispatch events only once.
     /// </summary>
-    internal class LiveEvent<TEventArgs>
+    public class LiveEvent<TEventArgs>
     {
-        private readonly MutableLiveData<TEventArgs> _liveData;
-        private readonly List<EventHandler<TEventArgs>> _subscribers = new List<EventHandler<TEventArgs>>();
+        /// <summary>
+        /// LiveData used to delegate the lifecycle-aware responsability.
+        /// </summary>
+        private readonly MutableLiveData<Event<TEventArgs>> _liveData = new MutableLiveData<Event<TEventArgs>>();
 
         public LiveEvent()
         {
-            _liveData = new MutableLiveData<TEventArgs>();
         }
 
         public LiveEvent(TEventArgs value)
         {
-            _liveData = new MutableLiveData<TEventArgs>(value);
+            _liveData.Value = new Event<TEventArgs>(value);
         }
 
-        public IDisposable Subscribe(ILifecycleOwner lifecycleOwner, EventHandler<TEventArgs> subscriber)
-        {            
-            _subscribers.Add(subscriber);
-            return Disposable.Create(() => _subscribers.Remove(subscriber));
+        /// <summary>
+        /// Subscribe to be notified of new events,
+        /// even if is already handled.
+        /// </summary>
+        /// <param name="lifecycleOwner">LifecycleOwner</param>
+        /// <param name="subscriber">Action delegate</param>
+        /// <returns>Unsubscription delegate.</returns>
+        public IDisposable Subscribe(ILifecycleOwner lifecycleOwner, Action<Event<TEventArgs>> subscriber) =>
+            Subscribe(lifecycleOwner, (_, args) => subscriber(args));
+
+        /// <summary>
+        /// Subscribe to be notified of new events,
+        /// even if is already handled.
+        /// </summary>
+        /// <param name="lifecycleOwner">LifecycleOwner</param>
+        /// <param name="subscriber">Action delegate</param>
+        /// <returns>Unsubscription delegate.</returns>
+        public IDisposable Subscribe(ILifecycleOwner lifecycleOwner, EventHandler<Event<TEventArgs>> subscriber)
+        {
+            return _liveData.BindMethod(lifecycleOwner, NotifySubscriber);
+
+            void NotifySubscriber(Event<TEventArgs> @event) => subscriber(this, @event);
+        }
+
+        /// <summary>
+        /// Subscribe to be notified only if the event is not handled.
+        /// It's useless to subscribe to multiple subscribers because the first
+        /// one will handle the event, and the subsequent ones will not be notified.
+        /// </summary>
+        /// <param name="lifecycleOwner">LifecycleOwner</param>
+        /// <param name="subscriber">Action delegate</param>
+        /// <returns>Unsubscription delegate.</returns>
+        public IDisposable SubscribeToExecuteIfUnhandled(ILifecycleOwner lifecycleOwner, Action<TEventArgs> subscriber) =>
+            SubscribeToExecuteIfUnhandled(lifecycleOwner, (_, args) => subscriber(args));
+
+        /// <summary>
+        /// Subscribe to be notified only if the event is not handled.
+        /// It's useless to subscribe to multiple subscribers because the first
+        /// one will handle the event, and the subsequent ones will not be notified.
+        /// </summary>
+        /// <param name="lifecycleOwner">LifecycleOwner</param>
+        /// <param name="subscriber">Action delegate</param>
+        /// <returns>Unsubscription delegate.</returns>
+        public IDisposable SubscribeToExecuteIfUnhandled(ILifecycleOwner lifecycleOwner, EventHandler<TEventArgs> subscriber)
+        {
+            return Subscribe(lifecycleOwner, UnwrapEventAndNotifySubscriber);
+
+            void UnwrapEventAndNotifySubscriber(object _, Event<TEventArgs> @event) => @event.ExecuteIfUnhandled(NotifySubscriber);
+            void NotifySubscriber(TEventArgs content) => subscriber(this, content);
         }
     }
 }
