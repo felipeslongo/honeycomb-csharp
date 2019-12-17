652387317756229a4d61f92cea3aede9b3b53e36
Felipe de Souza Longo <felipeslongo@gmail.com>
2019-12-17 00:46:57 -0300

    Create LifecycleObservable and MutableLifecycleObservable

---
+++ HoneyComb.Platform.iOS/Lifecycles/LifecycleObservable.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HoneyComb.Platform.iOS.Lifecycles
+{
+    /// <summary>
+    ///     iOS Lifecycle-aware observer.
+    ///     Use it as a Observable object to execute
+    ///     code that is lifecycle dependent.
+    /// </summary>
+    /// <remarks>
+    ///     Inspiration
+    ///     - LifecycleBoundObserver:
+    ///     https://android.googlesource.com/platform/frameworks/support/+/androidx-master-dev/lifecycle/lifecycle-livedata-core/src/main/java/androidx/lifecycle/LiveData.java
+    /// </remarks>
+    public class LifecycleObservable
+    {
+        public event EventHandler? LoadView;
+
+        public event EventHandler? LoadViewIfNeeded;
+
+        public event EventHandler? ViewDidLoad;
+
+        public event EventHandler? ViewWillAppear;
+
+        public event EventHandler? ViewWillLayoutSubviews;
+
+        public event EventHandler? ViewDidLayoutSubviews;
+
+        public event EventHandler? ViewDidAppear;
+
+        public event EventHandler? ViewWillDisappear;
+
+        public event EventHandler? ViewDidDisappear;
+
+        protected void InvokeLoadView() => LoadView?.Invoke(this, EventArgs.Empty);
+
+        protected void InvokeLoadViewIfNeeded() => LoadViewIfNeeded?.Invoke(this, EventArgs.Empty);
+
+        protected void InvokeViewDidLoad() => ViewDidLoad?.Invoke(this, EventArgs.Empty);
+
+        protected void InvokeViewWillAppear() => ViewWillAppear?.Invoke(this, EventArgs.Empty);
+
+        protected void InvokeViewWillLayoutSubviews() => ViewWillLayoutSubviews?.Invoke(this, EventArgs.Empty);
+
+        protected void InvokeViewDidLayoutSubviews() => ViewDidLayoutSubviews?.Invoke(this, EventArgs.Empty);
+
+        protected void InvokeViewDidAppear() => ViewDidAppear?.Invoke(this, EventArgs.Empty);
+
+        protected void InvokeViewWillDisappear() => ViewWillDisappear?.Invoke(this, EventArgs.Empty);
+
+        protected void InvokeViewDidDisappear() => ViewDidDisappear?.Invoke(this, EventArgs.Empty);
+    }
+}
---
+++ HoneyComb.Platform.iOS/Lifecycles/MutableLifecycleObservable.cs
@@ -0,0 +1,29 @@
+﻿namespace HoneyComb.Platform.iOS.Lifecycles
+{
+    /// <summary>
+    /// Implementation of <see cref="LifecycleObservable"/>
+    /// that allows direct manipulation of its state.
+    /// 
+    /// Do not expose this instance to your clients.
+    /// </summary>
+    public class MutableLifecycleObservable : LifecycleObservable
+    {
+        protected void NotifyLoadView() => InvokeLoadView();
+
+        protected void NotifyLoadViewIfNeeded() => InvokeLoadViewIfNeeded();
+
+        protected void NotifyViewDidLoad() => InvokeViewDidLoad();
+
+        protected void NotifyViewWillAppear() => InvokeViewWillAppear();
+
+        protected void NotifyViewWillLayoutSubviews() => InvokeViewWillLayoutSubviews();
+
+        protected void NotifyViewDidLayoutSubviews() => InvokeViewDidLayoutSubviews();
+
+        protected void NotifyViewDidAppear() => InvokeViewDidAppear();
+
+        protected void NotifyViewWillDisappear() => InvokeViewWillDisappear();
+
+        protected void NotifyViewDidDisappear() => InvokeViewDidDisappear();
+    }
+}
