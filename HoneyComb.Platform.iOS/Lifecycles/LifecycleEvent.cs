57e1a88f17f31aefdddf1e264e1b671d4485d94c
Felipe de Souza Longo <felipeslongo@gmail.com>
2019-12-17 01:14:10 -0300

    Create LifecycleEvent and LifecycleState

---
+++ HoneyComb.Platform.iOS/Lifecycles/LifecycleEvent.cs
@@ -0,0 +1,29 @@
+﻿namespace HoneyComb.Platform.iOS.Lifecycles
+{
+    /// <summary>
+    /// Lifecycle Events. You can consider the states as the nodes in a graph and Lifecycle.Events as the edges between these nodes.
+    /// </summary>
+    /// <remarks>
+    /// Credits: https://developer.android.com/reference/androidx/lifecycle/Lifecycle.Event.html
+    /// </remarks>
+    public enum LifecycleEvent
+    {
+        LoadView,
+
+        LoadViewIfNeeded,
+
+        ViewDidLoad,
+
+        ViewWillAppear,
+
+        ViewWillLayoutSubviews,
+
+        ViewDidLayoutSubviews,
+
+        ViewDidAppear,
+
+        ViewWillDisappear,
+
+        ViewDidDisappear,
+    }
+}
---
+++ HoneyComb.Platform.iOS/Lifecycles/LifecycleState.cs
@@ -0,0 +1,32 @@
+﻿namespace HoneyComb.Platform.iOS.Lifecycles
+{
+    /// <summary>
+    /// Lifecycle States. You can consider the states as the nodes in a graph and Lifecycle.Events as the edges between these nodes.
+    /// </summary>
+    /// <remarks>
+    /// Credits: https://developer.android.com/reference/androidx/lifecycle/Lifecycle.Event.html
+    /// </remarks>
+    public enum LifecycleState
+    {
+        /// <summary>
+        /// Initialized state for a LifecycleOwner. For an ViewController, this is the state when it is constructed but has not received ViewDidLoad yet.
+        /// </summary>
+        Initialized,
+        /// <summary>
+        /// Loaded state for a LifecycleOwner. For an ViewController, this is the state after ViewDidLoad was called but has not received ViewWillAppear yet.
+        /// </summary>
+        Loaded,
+        /// <summary>
+        /// Appeared state for a LifecycleOwner. For an ViewController, this is the state after ViewDidAppear was called.
+        /// </summary>
+        Appeared,
+        /// <summary>
+        /// Appeared state for a LifecycleOwner. For an ViewController, this is the state after ViewWillDisappear was called.
+        /// </summary>
+        Disappeared,
+        /// <summary>
+        /// Destroyed state for a LifecycleOwner. After this event, this Lifecycle will not dispatch any more events.
+        /// </summary>
+        Destroyed,
+    }
+}
