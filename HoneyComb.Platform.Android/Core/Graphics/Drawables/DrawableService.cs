fd3059c90658c6eee22d77cfb457807302b62e7a
Felipe de Souza Longo <felipeslongo@gmail.com>
2019-12-20 16:34:22 -0300

    Create BuildVersioDrawableService with CreateDrawableWithColorStateListTint
    Signed-off-by: Felipe de Souza Longo <felipeslongo@gmail.com>

---
+++ HoneyComb.Platform.Android/Core/Graphics/Drawables/DrawableService.cs
@@ -0,0 +1,30 @@
+using Android.Content.Res;
+using Android.Graphics.Drawables;
+using Android.Support.V4.Graphics.Drawable;
+
+namespace HoneyComb.Platform.Android.Core.Graphics.Drawables
+{
+    /// <summary>
+    ///     Provide helpers and utils for handling Drawables.
+    /// </summary>
+    public static class DrawableService
+    {
+        /// <summary>
+        ///     Creates a new Drawable with a ColorStateList applied to it as Tint.
+        /// </summary>
+        /// <remarks>
+        ///     Credits:
+        ///     https://stackoverflow.com/questions/26788251/android-tint-using-drawablecompat
+        ///     https://stackoverflow.com/questions/30872101/drawablecompat-tinting-does-not-work-on-pre-lollipop
+        /// </remarks>
+        /// <param name="drawable">Original drawable to be tinted</param>
+        /// <param name="color">Tint to be applied</param>
+        /// <returns>New Drawable instance with tint applied</returns>
+        public static Drawable CreateDrawableWithColorStateListTint(Drawable drawable, ColorStateList color)
+        {
+            var drawableWrapper = DrawableCompat.Wrap(drawable).Mutate();
+            DrawableCompat.SetTintList(drawableWrapper, color);
+            return drawableWrapper;
+        }
+    }
+}
