5bfc784797b0d926b879759b01bb3e8dabcf0d9b
Felipe de Souza Longo <felipeslongo@gmail.com>
2019-12-20 16:37:59 -0300

    Add SetCompoundDrawableTintListForApisBelow23
    Signed-off-by: Felipe de Souza Longo <felipeslongo@gmail.com>

--- HoneyComb.Platform.Android/Widget/TextView+SetCompoundDrawableTintList.cs
+++ HoneyComb.Platform.Android/Widget/TextView+SetCompoundDrawableTintList.cs
@@ -1,16 +1,45 @@
-﻿using Android.Widget;
+﻿using System;
+using System.Linq;
+using Android.Content.Res;
+using Android.Widget;
 using HoneyComb.Platform.Android.Content.Res;
+using HoneyComb.Platform.Android.Core.Graphics.Drawables;
+using HoneyComb.Platform.Android.OS;
 
 namespace HoneyComb.Platform.Android.Widget
 {
     public static class TextViewSetCompoundDrawableTintList
     {
         /// <summary>
-        /// Sets the left, right, top and bottom drawable tint.
+        ///     Sets the left, right, top and bottom drawable tint.
         /// </summary>
         /// <param name="this"></param>
         /// <param name="colorResourceId">Color state list resource</param>
-        public static void SetCompoundDrawableTintList(this TextView @this, int colorResourceId) =>
-            @this.CompoundDrawableTintList = ColorService.GetColorStateList(@this.Context, colorResourceId);
+        public static void SetCompoundDrawableTintList(this TextView @this, int colorResourceId)
+        {
+            var color = ColorService.GetColorStateList(@this.Context, colorResourceId);
+            if (BuildVersionService.IsBelowApi23())
+            {
+                @this.SetCompoundDrawableTintListForApisBelow23(color);
+                return;
+            }
+
+            @this.CompoundDrawableTintList = color;
+        }
+
+        /// <summary>
+        ///     Sets the the left, right, top and bottom drawable tint for devices below API 23
+        /// </summary>
+        /// <param name="this"></param>
+        /// <param name="color"></param>
+        private static void SetCompoundDrawableTintListForApisBelow23(this TextView @this, ColorStateList color)
+        {
+            throw new NotImplementedException();
+            //TODO Finish SetCompoundDrawableTintListForBelowApi23
+            foreach (var drawable in @this.GetCompoundDrawables().Where(d => d != null))
+            {
+                var tintedDrawable = DrawableService.CreateDrawableWithTint(drawable, color);
+            }
+        }
     }
 }
