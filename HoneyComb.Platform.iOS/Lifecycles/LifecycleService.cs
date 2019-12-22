1cf8f83070987ede8c7cc23ffe7d1ffd3add9673
Felipe de Souza Longo <felipeslongo@gmail.com>
2019-12-22 12:12:11 -0300

    Create LifecycleService.cs

---
+++ HoneyComb.Platform.iOS/Lifecycles/LifecycleService.cs
@@ -0,0 +1,12 @@
+﻿namespace HoneyComb.Platform.iOS.Lifecycles
+{
+    /// <summary>
+    /// Provides utilities and helpers related 
+    /// to iOS Lifecycle Components.
+    /// </summary>
+    public static class LifecycleService
+    {
+        public static LifecycleDisposable GetDisposable(LifecycleObservable lifecycleObservable) =>
+            new LifecycleDisposable(lifecycleObservable);
+    }
+}
