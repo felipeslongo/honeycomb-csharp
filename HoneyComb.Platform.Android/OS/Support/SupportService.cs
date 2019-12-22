93b820a5af04842600a9572df016ce0514d4425a
Felipe de Souza Longo <felipeslongo@gmail.com>
2019-12-22 10:57:49 -0300

    Create SupportService with TextViewSupportService (#14)

---
+++ HoneyComb.Platform.Android/OS/Support/SupportService.cs
@@ -0,0 +1,18 @@
+﻿using Android.Widget;
+
+namespace HoneyComb.Platform.Android.OS.Support
+{
+    /// <summary>
+    /// It provides flags that determine if an Android component feature
+    /// is supported by the current device Android OS version.
+    /// </summary>
+    public static class SupportService
+    {
+        /// <summary>
+        /// Gets SupportService for TextView.
+        /// </summary>
+        /// <param name="textView">TextView instance.</param>
+        /// <returns></returns>
+        public static TextViewSupportService GetSupport(TextView textView) => TextViewSupportService.Singleton;
+    }
+}
---
+++ HoneyComb.Platform.Android/OS/Support/TextViewSupportService.cs
@@ -0,0 +1,31 @@
+﻿using Android.Widget;
+
+namespace HoneyComb.Platform.Android.OS.Support
+{
+    /// <summary>
+    /// It provides flags that determine if an TextView feature
+    /// is supported by the current device Android OS version.
+    /// </summary>
+    public class TextViewSupportService
+    {
+        internal static TextViewSupportService Singleton = new TextViewSupportService();
+
+        private TextViewSupportService()
+        {
+        }
+
+        /// <summary>
+        /// Gets if <see cref="TextView.CompoundDrawableTintList"/> setter is supported.
+        /// </summary>
+        /// <param name="textView">TextView instance</param>
+        /// <returns>True if is supported, false otherwise</returns>
+        public bool IsCompoundDrawableTintListSetterSupported(TextView textView) => BuildVersionService.IsAtLeastApi23();
+
+        /// <summary>
+        /// Gets if <see cref="TextView.CompoundDrawableTintList"/> setter is unsupported.
+        /// </summary>
+        /// <param name="textView">TextView instance</param>
+        /// <returns>True if is unsupported, false otherwise</returns>
+        public bool IsCompoundDrawableTintListSetterUnsupported(TextView textView) => BuildVersionService.IsBelowApi23();
+    }
+}
