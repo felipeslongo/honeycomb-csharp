5dba804955dc2bfcb640ddf7e9f1244ec4ce90d0
Felipe de Souza Longo <felipeslongo@gmail.com>
2019-12-22 12:15:15 -0300

    Create LifecycleiOSHoneyComb.cs

---
+++ HoneyComb.Platform.iOS/Lifecycles/LifecycleiOSHoneyComb.cs
@@ -0,0 +1,72 @@
+﻿using HoneyComb.Core.Lifecycles;
+using System;
+
+namespace HoneyComb.Platform.iOS.Lifecycles
+{
+    /// <summary>
+    /// Android Lifecycle bound implementation of <see cref="Core.Lifecycles.Lifecycle"/>
+    /// </summary>
+    public sealed class LifecycleiOSHoneyComb : Lifecycle
+    {
+        private LifecycleDisposable _lifecycleDisposable;
+
+        public LifecycleiOSHoneyComb(ILifecycleOwner owner, LifecycleObservable lifecycleObservable) : base(owner)
+        {
+            SynchronizeUpToCurrentState(lifecycleObservable.StateLastKnown);
+
+            lifecycleObservable.ViewWillAppear += LifecycleObservable_ViewWillOrDidAppear;
+            lifecycleObservable.ViewDidAppear += LifecycleObservable_ViewWillOrDidAppear;
+
+            lifecycleObservable.ViewWillDisappear += LifecycleObservable_ViewWillOrDidDisappear;
+            lifecycleObservable.ViewDidDisappear += LifecycleObservable_ViewWillOrDidDisappear;
+
+            _lifecycleDisposable = LifecycleService.GetDisposable(lifecycleObservable);
+            _lifecycleDisposable.Add(() =>
+            {
+                if (lifecycleObservable is null)
+                    return;
+
+                lifecycleObservable.ViewWillAppear -= LifecycleObservable_ViewWillOrDidAppear;
+                lifecycleObservable.ViewDidAppear -= LifecycleObservable_ViewWillOrDidAppear;
+
+                lifecycleObservable.ViewWillDisappear -= LifecycleObservable_ViewWillOrDidDisappear;
+                lifecycleObservable.ViewDidDisappear -= LifecycleObservable_ViewWillOrDidDisappear;
+
+                NotifyStateChange(LifecycleState.Disposed);
+                Dispose();
+            });
+        }
+
+        private void LifecycleObservable_ViewWillOrDidAppear(object sender, EventArgs e) => NotifyStateChange(LifecycleState.Active);
+
+        private void LifecycleObservable_ViewWillOrDidDisappear(object sender, EventArgs e) => NotifyStateChange(LifecycleState.Inactive);
+
+        private void SynchronizeUpToCurrentState(iOSLifecycleState state)
+        {
+            switch (state)
+            {
+                case iOSLifecycleState.Initialized:
+                    break;
+
+                case iOSLifecycleState.Loaded:
+                    break;
+
+                case iOSLifecycleState.Appeared:
+                    NotifyStateChange(LifecycleState.Active);
+                    break;
+
+                case iOSLifecycleState.Disappeared:
+                    NotifyStateChange(LifecycleState.Inactive);
+                    break;
+
+                case iOSLifecycleState.Destroyed:
+                    NotifyStateChange(LifecycleState.Inactive);
+                    NotifyStateChange(LifecycleState.Disposed);
+                    break;
+
+                default:
+                    break;
+            }
+        }
+    }
+}
