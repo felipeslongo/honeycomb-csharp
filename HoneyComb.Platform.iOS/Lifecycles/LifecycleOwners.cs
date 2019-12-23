22d821a49091f686a491aaf2c1199af2e4e8a558
Felipe de Souza Longo <felipeslongo@gmail.com>
2019-12-22 23:56:20 -0300

    Create LifecycleOwners.cs

---
+++ HoneyComb.Platform.iOS/Lifecycles/LifecycleOwners.cs
@@ -0,0 +1,23 @@
+﻿using HoneyComb.Core.Lifecycles;
+
+namespace HoneyComb.Platform.iOS.Lifecycles
+{
+    /// <summary>
+    ///     Companion object that encapsulates
+    ///     both LifecyclerOwners from iOS (currently not done yet)
+    ///     and from Honeycomb
+    /// </summary>
+    public class LifecycleOwners
+    {
+        public LifecycleOwners(LifecycleObservable lifecycleObservable)
+        {
+            Observable = lifecycleObservable;
+            Disposable = LifecycleService.GetDisposable(Observable);
+            HoneyComb = LifecycleService.GetHoneyCombLifecycleOwner(Observable);
+        }
+
+        public LifecycleDisposable Disposable { get; }
+        public ILifecycleOwner HoneyComb { get; }
+        public LifecycleObservable Observable { get; }
+    }
+}
