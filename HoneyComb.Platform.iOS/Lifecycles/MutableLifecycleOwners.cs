356520b5c8e0e27724016270c084c687e709e058
Felipe de Souza Longo <felipeslongo@gmail.com>
2019-12-22 23:59:26 -0300

    Create MutableLifecycleOwners.cs

---
+++ HoneyComb.Platform.iOS/Lifecycles/MutableLifecycleOwners.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HoneyComb.Platform.iOS.Lifecycles
+{
+    /// <summary>
+    ///     Companion object that encapsulates
+    ///     both LifecyclerOwners from iOS (currently not done yet)
+    ///     and from Honeycomb.
+    ///     
+    ///     Only the UIViewController that owns this instance
+    ///     should touch it. Other objects should not be exposed to it.
+    /// </summary>
+    public class MutableLifecycleOwners : LifecycleOwners
+    {
+        public MutableLifecycleOwners(MutableLifecycleObservable lifecycleObservable) : base(lifecycleObservable)
+        {
+            MutableObservable = lifecycleObservable;
+    }
+
+        public MutableLifecycleObservable MutableObservable { get; }
+    }
+}
