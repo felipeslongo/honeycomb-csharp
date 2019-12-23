e671165982029426e17b65313e0a0a135b5cc8f2
Felipe de Souza Longo <felipeslongo@gmail.com>
2019-12-23 00:21:34 -0300

    Create MutableLifecycleObservableSimplified.cs

---
+++ HoneyComb.Platform.iOS/Lifecycles/MutableLifecycleObservableSimplified.cs
@@ -0,0 +1,25 @@
+﻿namespace HoneyComb.Platform.iOS.Lifecycles
+{
+    /// <summary>
+    ///     Simplified facade/wrapper around <see cref="MutableLifecycleObservable"/>
+    ///     that exposes the bare minimum lifecycle notifications that you MUST
+    ///     call in your UIViewController in order to comply to HoneyComb Lifecycle.
+    ///     
+    ///     Keep note that you will have an <see cref="LifecycleObservable"/>
+    ///     instance that will not dispatch events for every single lifecycle event of iOS
+    ///     if you choose to use this slim/simplified approach.
+    /// </summary>
+    public class MutableLifecycleObservableSimplified
+    {
+        private MutableLifecycleObservable mutableObservable;
+
+        public MutableLifecycleObservableSimplified(MutableLifecycleObservable mutableObservable)
+        {
+            this.mutableObservable = mutableObservable;
+        }
+
+        public void NotifyViewDidAppear() => mutableObservable.NotifyViewDidAppear();
+        public void NotifyViewWillDisappear(UIKit.UIViewController controller) => mutableObservable.NotifyViewWillDisappear(controller);
+        public void NotifyViewDidDisappear() => mutableObservable.NotifyViewDidDisappear();
+    }
+}
