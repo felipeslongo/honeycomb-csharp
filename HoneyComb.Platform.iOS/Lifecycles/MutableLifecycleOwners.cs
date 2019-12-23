7789b7ca3bb488e7b38a0f18ce3774d2009284d8
Felipe de Souza Longo <felipeslongo@gmail.com>
2019-12-23 00:21:56 -0300

    Update MutableLifecycleOwners add MutableLifecycleObservableSimplified

--- HoneyComb.Platform.iOS/Lifecycles/MutableLifecycleOwners.cs
+++ HoneyComb.Platform.iOS/Lifecycles/MutableLifecycleOwners.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace HoneyComb.Platform.iOS.Lifecycles
+﻿namespace HoneyComb.Platform.iOS.Lifecycles
 {
     /// <summary>
     ///     Companion object that encapsulates
@@ -11,14 +7,27 @@ namespace HoneyComb.Platform.iOS.Lifecycles
     ///     
     ///     Only the UIViewController that owns this instance
     ///     should touch it. Other objects should not be exposed to it.
+    ///     
+    ///     Use it to favor Composition over Inheritance.
     /// </summary>
     public class MutableLifecycleOwners : LifecycleOwners
     {
         public MutableLifecycleOwners(MutableLifecycleObservable lifecycleObservable) : base(lifecycleObservable)
         {
             MutableObservable = lifecycleObservable;
+            SimplifiedObservable = new MutableLifecycleObservableSimplified(MutableObservable);
     }
 
+        /// <summary>
+        /// Gets a mutable LifecycleObservable that you can use to notify
+        /// lifecycle events of your UIViewController.
+        /// </summary>
         public MutableLifecycleObservable MutableObservable { get; }
+
+        /// <summary>
+        /// Gets a simplified mutable LifecycleObservable that you can use to notify
+        /// the bare minimum lifecycle events of your UIViewController.
+        /// </summary>
+        public MutableLifecycleObservableSimplified SimplifiedObservable { get; }
     }
 }
