643f84f10da214dae4f90e3e2e9e6f8ab9f105b7
Felipe de Souza Longo <felipeslongo@gmail.com>
2020-01-05 18:29:39 -0300

    Update LifecycleiOSHoneyComb to observe application global lifecycle

--- HoneyComb.Platform.iOS/Lifecycles/LifecycleiOSHoneyComb.cs
+++ HoneyComb.Platform.iOS/Lifecycles/LifecycleiOSHoneyComb.cs
@@ -20,6 +20,7 @@ namespace HoneyComb.Platform.iOS.Lifecycles
             lifecycleObservable.ViewWillDisappear += LifecycleObservable_ViewWillOrDidDisappear;
             lifecycleObservable.ViewDidDisappear += LifecycleObservable_ViewWillOrDidDisappear;
 
+            var appLifecycleSubscription = ObserveAppLifecycle(LifecycleService.GetAppLifecycleObservable(), lifecycleObservable);
             _lifecycleDisposable = LifecycleService.GetDisposable(lifecycleObservable);
             _lifecycleDisposable.Add(() =>
             {
@@ -32,6 +33,8 @@ namespace HoneyComb.Platform.iOS.Lifecycles
                 lifecycleObservable.ViewWillDisappear -= LifecycleObservable_ViewWillOrDidDisappear;
                 lifecycleObservable.ViewDidDisappear -= LifecycleObservable_ViewWillOrDidDisappear;
 
+                appLifecycleSubscription();
+
                 NotifyStateChange(LifecycleState.Disposed);
                 Dispose();
             });
@@ -40,6 +43,29 @@ namespace HoneyComb.Platform.iOS.Lifecycles
         private void LifecycleObservable_ViewWillOrDidAppear(object sender, EventArgs e) => NotifyStateChange(LifecycleState.Active);
 
         private void LifecycleObservable_ViewWillOrDidDisappear(object sender, EventArgs e) => NotifyStateChange(LifecycleState.Inactive);
+
+        /// <summary>
+        /// Observes the Application Global Lifecycle in order to dispatch updates for inactive state
+        /// when the app goes into background...
+        /// </summary>
+        /// <param name="appLifecycle"></param>
+        /// <param name="viewControllerLifecycle"></param>
+        /// <returns></returns>
+        private Action ObserveAppLifecycle(AppLifecycleObservable appLifecycle, LifecycleObservable viewControllerLifecycle)
+        {
+            appLifecycle.WillResignActive += OnWillResignActive;
+            appLifecycle.DidBecomeActive += OnDidBecomeActive;
+
+            return () =>
+            {
+                appLifecycle.WillResignActive -= OnWillResignActive;
+                appLifecycle.DidBecomeActive -= OnDidBecomeActive;
+            };
+
+            void OnWillResignActive(object sender, EventArgs e) => NotifyStateChange(LifecycleState.Inactive);
+
+            void OnDidBecomeActive(object sender, EventArgs e) => SynchronizeUpToCurrentState(viewControllerLifecycle.StateLastKnown);
+        }
 
         private void SynchronizeUpToCurrentState(iOSLifecycleState state)
         {
