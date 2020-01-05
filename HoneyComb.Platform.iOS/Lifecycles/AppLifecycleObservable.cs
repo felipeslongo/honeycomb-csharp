f8cf014898db92b428ac5ae020efe3b7f29ca61e
Felipe de Souza Longo <felipeslongo@gmail.com>
2020-01-05 17:19:31 -0300

    Update AppLifecycleObservable observe application lifecycle notifications

--- HoneyComb.Platform.iOS/Lifecycles/AppLifecycleObservable.cs
+++ HoneyComb.Platform.iOS/Lifecycles/AppLifecycleObservable.cs
@@ -1,4 +1,6 @@
 using Foundation;
+using System;
+using System.Collections.Generic;
 using UIKit;
 
 namespace HoneyComb.Platform.iOS.Lifecycles
@@ -6,19 +8,20 @@ namespace HoneyComb.Platform.iOS.Lifecycles
     /// <summary>
     ///     iOS Application Global Lifecycle-aware observer.
     ///     Use it as a Observable object to execute
-    ///     code that is lifecycle dependent of the App lifecycle.
+    ///     code that is lifecycle dependent of the Application Global lifecycle.
     /// </summary>
     public sealed class AppLifecycleObservable
     {
-        public AppLifecycleObservable()
+        private AppLifecycleObservable()
         {
-            NSNotificationCenter.DefaultCenter.AddObserver(,);
+            SubscribeToUIApplicationLifecycle();
         }
-        
-        /// <summary>
-        ///     Gets the Global Application Lifecycle <see cref="UIApplicationState" /> current state.
-        /// </summary>
-        public UIApplicationState StateCurrent => UIApplication.SharedApplication.ApplicationState;
+
+        public event EventHandler? DidBecomeActive;
+
+        public event EventHandler? WillResignActive;
+
+        public event EventHandler? WillTerminate;
 
         /// <summary>
         ///     Check if the app is running in the background
@@ -26,5 +29,65 @@ namespace HoneyComb.Platform.iOS.Lifecycles
         /// <remarks>https://stackoverflow.com/questions/5835806/is-there-any-way-to-check-if-ios-app-is-in-background</remarks>
         public bool IsInBackground => StateCurrent == UIApplicationState.Background ||
                                       StateCurrent == UIApplicationState.Inactive;
+
+        /// <summary>
+        ///     Check if the app is running in the foreground as Active.
+        /// </summary>
+        /// <remarks>https://stackoverflow.com/questions/5835806/is-there-any-way-to-check-if-ios-app-is-in-background</remarks>
+        public bool IsInForeground => StateCurrent == UIApplicationState.Active;
+
+        /// <summary>
+        ///     Gets the Global Application Lifecycle <see cref="UIApplicationState" /> current state.
+        /// </summary>
+        public UIApplicationState StateCurrent => UIApplication.SharedApplication.ApplicationState;
+
+        private void Dispose()
+        {
+            DidBecomeActive = null;
+            WillResignActive = null;
+            WillTerminate = null;
+        }
+
+        /// <summary>
+        /// Subscribe to the <see cref="UIApplication"/> lifecycle related notifications.
+        /// </summary>
+        /// <remarks>
+        ///     Credits
+        ///         https://stackoverflow.com/questions/9011868/whats-the-best-way-to-detect-when-the-app-is-entering-the-background-for-my-vie
+        ///         https://stackoverflow.com/questions/34744783/detect-ios-app-entering-background
+        /// </remarks>
+        private void SubscribeToUIApplicationLifecycle()
+        {
+            var subscriptionTokens = new List<IDisposable>();
+            subscriptionTokens.Add(SubscribeToUIApplicationDidBecomeActive());
+            subscriptionTokens.Add(SubscribeToUIApplicationWillResignActive());
+            subscriptionTokens.Add(SubscribeToUIApplicationWillTerminate());
+
+            IDisposable SubscribeToUIApplicationDidBecomeActive() =>
+                NSNotificationCenter.DefaultCenter.AddObserver(UIApplication.DidBecomeActiveNotification, OnUIApplicationDidBecomeActive);
+
+            IDisposable SubscribeToUIApplicationWillResignActive() =>
+            NSNotificationCenter.DefaultCenter.AddObserver(UIApplication.WillResignActiveNotification, OnUIApplicationWillResignActive);
+
+            IDisposable SubscribeToUIApplicationWillTerminate() =>
+                NSNotificationCenter.DefaultCenter.AddObserver(UIApplication.WillTerminateNotification, OnUIApplicationWillTerminate);
+
+            void OnUIApplicationDidBecomeActive(NSNotification _)
+            {
+                DidBecomeActive?.Invoke(this, EventArgs.Empty);
+            }
+
+            void OnUIApplicationWillResignActive(NSNotification _)
+            {
+                WillResignActive?.Invoke(this, EventArgs.Empty);
+            }
+
+            void OnUIApplicationWillTerminate(NSNotification _)
+            {
+                WillTerminate?.Invoke(this, EventArgs.Empty);
+                subscriptionTokens.ForEach(subscription => subscription.Dispose());
+                Dispose();
+            }
+        }
     }
 }
