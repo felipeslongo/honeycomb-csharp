7315b98f92a50bab1924117da2da5c6a63cf5c74
Felipe de Souza Longo <felipeslongo@gmail.com>
2020-01-12 17:37:18 -0300

    Remove ChangeState exceptions Guard (#19)
    * Modify LifecycleState+ChangeState.cs remove state check exceptions
    
    * Remove GivenAnInvalidTransition_ShouldFail
    
    * Bump Core to 1.0.0-beta04
    
    * Modify LifecycleState+ChangeState.cs add ChangeStateStrict

--- HoneyComb.Platform.System.Tests/Lifecycles/LifecycleState+ChangeStateTests.cs
+++ HoneyComb.Platform.System.Tests/Lifecycles/LifecycleState+ChangeStateTests.cs
@@ -16,35 +16,35 @@ namespace HoneyComb.Core.Tests.Lifecycles
             }
         }
 
-        public class ChangeStateTests : LifecycleState_ChangeStateTests
+        public class ChangeStateStrictTests : LifecycleState_ChangeStateTests
         {
             [Theory, Trait(nameof(Category), Category.Unit)]
             [MemberData(nameof(EnumValues))]
             public void GivenAnyState_WhenChangeStateToSame_ShouldSucceed(LifecycleState state)
             {
-                var actual = state.ChangeState(state);
-
+                var actual = state.ChangeStateStrict(state);
+        
                 Assert.Equal(state, actual);
             }
-
+        
             [Theory, Trait(nameof(Category), Category.Unit)]
             [MemberData(nameof(EnumValues))]
             public void GivenAnyState_WhenChangeStateToInitialized_ShouldFail(LifecycleState state)
             {
                 if (state == LifecycleState.Initialized)
                     return;
-                Assert.Throws<InvalidOperationException>(() => state.ChangeState(LifecycleState.Initialized));
+                Assert.Throws<InvalidOperationException>(() => state.ChangeStateStrict(LifecycleState.Initialized));
             }
-
+        
             [Theory, Trait(nameof(Category), Category.Unit)]
             [MemberData(nameof(EnumValues))]
             public void GivenDisposedState_WhenChangeStateToAny_ShouldFail(LifecycleState state)
             {
                 if (state == LifecycleState.Disposed)
                     return;
-                Assert.Throws<InvalidOperationException>(() => LifecycleState.Disposed.ChangeState(state));
+                Assert.Throws<InvalidOperationException>(() => LifecycleState.Disposed.ChangeStateStrict(state));
             }
-
+        
             [Theory, Trait(nameof(Category), Category.Unit)]
             [InlineData(LifecycleState.Initialized, LifecycleState.Active)]
             [InlineData(LifecycleState.Active, LifecycleState.Inactive)]
@@ -52,11 +52,11 @@ namespace HoneyComb.Core.Tests.Lifecycles
             [InlineData(LifecycleState.Inactive, LifecycleState.Disposed)]
             public void GivenValidTransitions_WhenChangeState_ShouldSucceed(LifecycleState fromState, LifecycleState toState)
             {
-                var actual = fromState.ChangeState(toState);
-
+                var actual = fromState.ChangeStateStrict(toState);
+        
                 Assert.Equal(toState, actual);
             }
-
+        
             [Theory, Trait(nameof(Category), Category.Unit)]
             [InlineData(LifecycleState.Initialized, LifecycleState.Inactive)]
             [InlineData(LifecycleState.Initialized, LifecycleState.Disposed)]
@@ -68,7 +68,7 @@ namespace HoneyComb.Core.Tests.Lifecycles
             [InlineData(LifecycleState.Disposed, LifecycleState.Inactive)]
             public void GivenInvalidTransitions_WhenChangeState_ShouldFail(LifecycleState fromState, LifecycleState toState)
             {
-                Assert.Throws<InvalidOperationException>(() => fromState.ChangeState(toState));
+                Assert.Throws<InvalidOperationException>(() => fromState.ChangeStateStrict(toState));
             }
         }
     }
