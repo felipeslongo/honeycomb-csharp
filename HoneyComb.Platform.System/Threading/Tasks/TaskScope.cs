61b587082fa608e0e483c9877471db70c8eba0af
Felipe de Souza Longo <felipeslongo@gmail.com>
2020-01-25 11:52:23 -0300

    Create TaskScopeFactory and TaskScope

---
+++ HoneyComb.Platform.System/Threading/Tasks/TaskScope.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading;
+
+namespace HoneyComb.Core.Threading.Tasks
+{
+    /// <summary>
+    /// Basic implementation of <see cref="ITaskScope"/>.
+    /// </summary>
+    internal class TaskScope : ITaskScope
+    {
+        private readonly CancellationTokenSource tokenSource = new CancellationTokenSource();
+
+        public CancellationToken CancellationToken => tokenSource.Token;
+    }
+}
