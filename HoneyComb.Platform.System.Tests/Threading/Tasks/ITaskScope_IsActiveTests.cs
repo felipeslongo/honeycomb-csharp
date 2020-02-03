1ca835dbea3d5d2ab03168b2edd24d228edeb1b4
Felipe de Souza Longo <felipeslongo@gmail.com>
2020-02-02 22:27:38 -0300

    Create ITaskScope+IsActive

---
+++ HoneyComb.Platform.System.Tests/Threading/Tasks/ITaskScope_IsActiveTests.cs
@@ -0,0 +1,51 @@
+﻿using HoneyComb.Core.Threading.Tasks;
+using HoneyComb.TestChamber;
+using System;
+using Xunit;
+
+namespace HoneyComb.Core.Tests.Threading.Tasks
+{
+    public class ITaskScope_IsActiveTests
+    {
+        private readonly ITaskScopeMutable taskScope;
+
+        public ITaskScope_IsActiveTests()
+        {
+            taskScope = TaskScopeFactory.Create();
+        }
+
+        [Fact]
+        [Trait(nameof(Category), Category.Unit)]
+        public void GivenACanceledScope_WhenInvoked_ShouldBeFalse()
+        {
+            taskScope.Cancel();
+
+            Assert.False(taskScope.IsActive());
+        }
+
+        [Fact]
+        [Trait(nameof(Category), Category.Unit)]
+        public void GivenAFaultedScope_WhenInvoked_ShouldBeFalse()
+        {
+            taskScope.FinishWithException(new ApplicationException());
+
+            Assert.False(taskScope.IsActive());
+        }
+
+        [Fact]
+        [Trait(nameof(Category), Category.Unit)]
+        public void GivenAFinishedScope_WhenInvoked_ShouldBeFalse()
+        {
+            taskScope.Finish();
+
+            Assert.False(taskScope.IsActive());
+        }
+
+        [Fact]
+        [Trait(nameof(Category), Category.Unit)]
+        public void GivenAUnfinishedScope_WhenInvoked_ShouldBeTrue()
+        {
+            Assert.True(taskScope.IsActive());
+        }
+    }
+}
