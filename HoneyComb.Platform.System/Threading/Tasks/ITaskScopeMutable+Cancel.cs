8a5d66b6ac59534f3d6c5d6eec903e6c1a6a04ef
Felipe de Souza Longo <felipeslongo@gmail.com>
2020-02-02 23:24:10 -0300

    Create ITaskScopeMutable extensions for cancellation, finish, and exception

--- HoneyComb.Platform.System/Threading/Tasks/ITaskScopeMutable+Cancel.cs
+++ HoneyComb.Platform.System/Threading/Tasks/ITaskScopeMutable+Cancel.cs
@@ -7,8 +7,28 @@
     {
         /// <summary>
         ///     Communicates a request for cancellation of this scope's tasks.
+        ///     Transitions the <see cref="ITaskScope.ScopeTask"/> into 
+        ///     the <see cref="System.Threading.Tasks.TaskStatus.Canceled"/> state.
         /// </summary>
         /// <param name="this"></param>
-        public static void Cancel(this ITaskScopeMutable @this) => @this.CancellationTokenSource.Cancel();
+        public static void Cancel(this ITaskScopeMutable @this)
+        {
+            @this.CancellationTokenSource.Cancel();
+            @this.ScopeTaskCompletionSource.SetCanceled();
+        }
+
+        /// <summary>
+        ///     Try to communicates a request for cancellation of this scope's tasks.
+        ///     Transitions the <see cref="ITaskScope.ScopeTask"/> into 
+        ///     the <see cref="System.Threading.Tasks.TaskStatus.Canceled"/> state.
+        ///     
+        ///     If the status in a completed state, it does nothing.
+        /// </summary>
+        /// <param name="this"></param>
+        public static void TryCancel(this ITaskScopeMutable @this)
+        {
+            @this.CancellationTokenSource.Cancel();
+            @this.ScopeTaskCompletionSource.TrySetCanceled();
+        }
     }
 }
---
+++ HoneyComb.Platform.System/Threading/Tasks/ITaskScopeMutable+FinishWithException.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace HoneyComb.Core.Threading.Tasks
+{
+    /// <summary>
+    /// Extension for ITaskScopeMutable.FinishWithException
+    /// </summary>
+    public static class ITaskScopeMutable_FinishWithException
+    {
+        /// <summary>
+        ///     Transitions the <see cref="ITaskScope.ScopeTask"/> into the <see cref="System.Threading.Tasks.TaskStatus.Faulted"/>
+        ///     state and binds it to a specified exception.
+        /// </summary>
+        /// <param name="this"></param>
+        /// <param name="e"></param>
+        public static void FinishWithException(this ITaskScopeMutable @this, Exception e) 
+        {
+            @this.CancellationTokenSource.Cancel();
+            @this.ScopeTaskCompletionSource.SetException(e);
+        }
+
+        /// <summary>
+        ///     Transitions the <see cref="ITaskScope.ScopeTask"/> into the <see cref="System.Threading.Tasks.TaskStatus.Faulted"/>
+        ///     state and binds it to a specified exception.
+        ///     
+        ///     If the status in a completed state, it does nothing.
+        /// </summary>
+        /// <param name="this"></param>
+        /// <param name="e"></param>
+        public static void TryFinishWithException(this ITaskScopeMutable @this, Exception e)
+        {
+            @this.CancellationTokenSource.Cancel();
+            @this.ScopeTaskCompletionSource.TrySetException(e);
+        }
+    }
+}
