a9950c5dc79ef361316aac6b56acd045d7cc4cbf
Felipe de Souza Longo <felipeslongo@gmail.com>
2020-04-20 16:39:23 -0300

    Create IsBusy.cs

---
+++ HoneyComb.Platform.System/Lifecycles/IsBusy.cs
@@ -0,0 +1,37 @@
+﻿using HoneyComb.Core.Threading;
+using System;
+using System.Reactive.Disposables;
+using System.Threading.Tasks;
+
+namespace HoneyComb.Core.Lifecycles
+{
+    public sealed class IsBusy : IDisposable
+    {
+        private readonly MutexLockAsync throttler;
+
+        public IsBusy(bool isBusy = false)
+        {
+            throttler = new MutexLockAsync(isBusy);
+        }
+
+        public bool Value => throttler.IsLocked;
+
+        public static implicit operator bool(IsBusy @this) => @this.Value;
+
+        public void Dispose()
+        {
+            throttler.Dispose();
+        }
+
+        public void Release()
+        {
+            throttler.Release();
+        }
+
+        public async Task<IDisposable> WaitAsync()
+        {
+            await WaitAsync().ConfigureAwait(false);
+            return Disposable.Create(Release);
+        }
+    }
+}
