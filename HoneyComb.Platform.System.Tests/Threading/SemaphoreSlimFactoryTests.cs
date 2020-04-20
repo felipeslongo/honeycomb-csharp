9e2c3a4e3fbe3355614589020db9da59ca6206e2
Felipe de Souza Longo <felipeslongo@gmail.com>
2020-04-20 15:26:40 -0300

    Create SemaphoreSlimFactory

---
+++ HoneyComb.Platform.System.Tests/Threading/SemaphoreSlimFactoryTests.cs
@@ -0,0 +1,40 @@
+﻿using HoneyComb.Core.Threading;
+using HoneyComb.TestChamber;
+using System.Threading;
+using Xunit;
+
+namespace HoneyComb.Core.Tests.Threading
+{
+    public class SemaphoreSlimFactoryTests
+    {
+        public class CreateMutexLockTests : SemaphoreSlimFactoryTests
+        {
+            [Fact]
+            [Trait(nameof(Category), Category.Unit)]
+            public void GivenNoParameter_WhenInvoked_ShouldReturnASemaphoreWithCurrentCountOfOne()
+            {
+                var semaphore = SemaphoreSlimFactory.CreateMutexLock();
+
+                Assert.Equal(1, semaphore.CurrentCount);
+            }
+
+            [Fact]
+            [Trait(nameof(Category), Category.Unit)]
+            public void GivenNoParameter_WhenInvoked_ShouldReturnASemaphoreWithMaxCountOfOne()
+            {
+                var semaphore = SemaphoreSlimFactory.CreateMutexLock();
+
+                Assert.Throws<SemaphoreFullException>(() => semaphore.Release());
+            }
+
+            [Fact]
+            [Trait(nameof(Category), Category.Unit)]
+            public void GivenTrueParameter_WhenInvoked_ShouldReturnASemaphoreWithCurrentCountOfZero()
+            {
+                var semaphore = SemaphoreSlimFactory.CreateMutexLock(true);
+
+                Assert.Equal(0, semaphore.CurrentCount);
+            }
+        }
+    }
+}
