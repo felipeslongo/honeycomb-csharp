5804e60e5e5d939b66f55607e686cf5414e7ec08
Felipe de Souza Longo <felipeslongo@gmail.com>
2020-04-20 15:43:43 -0300

    Create MutexLockAsync

---
+++ HoneyComb.Platform.System.Tests/Threading/MutexLockAsyncTests.cs
@@ -0,0 +1,49 @@
+﻿using HoneyComb.Core.Threading;
+using HoneyComb.TestChamber;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace HoneyComb.Core.Tests.Threading
+{
+    public class MutexLockAsyncTests
+    {
+        public class WaitAsyncTests : MutexLockAsyncTests
+        {
+            [Fact]
+            [Trait(nameof(Category), Category.Unit)]
+            public async Task GivenAMutexLockAsync_WhenInvoked_ShouldBeLocked()
+            {
+                using var mutexLock = new MutexLockAsync();
+
+                using (await mutexLock.WaitAsync())
+                {
+                    Assert.True(mutexLock.IsLocked);
+                }
+            }
+
+            [Fact]
+            [Trait(nameof(Category), Category.Unit)]
+            public async Task GivenAMutexLockAsyncInLockedState_WhenLockIsDisposed_ShouldBeUnlocked()
+            {
+                using var mutexLock = new MutexLockAsync();
+                var disposable = await mutexLock.WaitAsync();
+
+                disposable.Dispose();
+
+                Assert.False(mutexLock.IsLocked);
+            }
+
+            [Fact]
+            [Trait(nameof(Category), Category.Unit)]
+            public async Task GivenAMutexLockAsyncInLockedState_WhenTryingToEnter_ShouldBeBeLockedInAwaitState()
+            {
+                using var mutexLock = new MutexLockAsync();
+                using var disposable = await mutexLock.WaitAsync();
+
+                var lockAsyncTask = mutexLock.WaitAsync();
+
+                Assert.False(lockAsyncTask.IsCompleted);
+            }
+        }
+    }
+}
