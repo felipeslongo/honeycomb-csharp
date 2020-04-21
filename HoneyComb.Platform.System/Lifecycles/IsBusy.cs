859286125933a8a2f6c1e659d782cc1012c8a5d7
Felipe de Souza Longo <felipeslongo@gmail.com>
2020-04-21 09:45:29 -0300

    Update IsBusy to add IObservable<bool> feature

--- HoneyComb.Platform.System/Lifecycles/IsBusy.cs
+++ HoneyComb.Platform.System/Lifecycles/IsBusy.cs
@@ -1,6 +1,7 @@
 ﻿using HoneyComb.Core.Threading;
 using System;
 using System.Reactive.Disposables;
+using System.Reactive.Linq;
 using System.Threading.Tasks;
 
 namespace HoneyComb.Core.Lifecycles
@@ -9,14 +10,21 @@ namespace HoneyComb.Core.Lifecycles
     ///     Represents the concept of an Busy state
     ///     to be used in a ViewModel.
     /// </summary>
-    public sealed class IsBusy : IDisposable
+    public sealed class IsBusy : IDisposable, IObservable<bool>
     {
         private readonly MutexLockAsync throttler;
+        private readonly Lazy<IObservable<bool>> asObservable;
 
         public IsBusy(bool isBusy = false)
         {
             throttler = new MutexLockAsync(isBusy);
+            asObservable = new Lazy<IObservable<bool>>(() => Observable.FromEventPattern<bool>(
+                eventHandler => ValueChanged += eventHandler,
+                eventHandler => ValueChanged -= eventHandler
+                ).Select(eventPattern => eventPattern.EventArgs));
         }
+
+        public event EventHandler<bool>? ValueChanged;
 
         public bool Value => throttler.IsLocked;
 
@@ -29,13 +37,17 @@ namespace HoneyComb.Core.Lifecycles
 
         public void Release()
         {
+            ValueChanged?.Invoke(this, false);
             throttler.Release();
         }
 
         public async Task<IDisposable> WaitAsync()
         {
-            await WaitAsync().ConfigureAwait(false);
+            await throttler.WaitAsync().ConfigureAwait(false);
+            ValueChanged?.Invoke(this, true);
             return Disposable.Create(Release);
         }
+
+        public IDisposable Subscribe(IObserver<bool> observer) => asObservable.Value.Subscribe(observer);
     }
 }
