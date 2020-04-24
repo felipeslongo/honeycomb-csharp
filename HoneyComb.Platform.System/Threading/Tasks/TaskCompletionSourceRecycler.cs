4c7d74231318ae9bf76fcbf69234bab5489f4bf9
Felipe de Souza Longo <felipeslongo@gmail.com>
2020-04-24 11:54:52 -0300

    Create RecycleAsync in TaskCompletionSourceRecycler

--- HoneyComb.Platform.System/Threading/Tasks/TaskCompletionSourceRecycler.cs
+++ HoneyComb.Platform.System/Threading/Tasks/TaskCompletionSourceRecycler.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HoneyComb.Platform.System.Threading.Tasks;
+using System;
 using System.Threading.Tasks;
 
 namespace HoneyComb.Core.Threading.Tasks
@@ -18,12 +19,26 @@ namespace HoneyComb.Core.Threading.Tasks
         {
             taskSource.TrySetResult(result);
             taskSource = new TaskCompletionSource<TResult>();
-        }
+        }      
 
         public void Recycle(Exception exception)
         {
             taskSource.TrySetException(exception);
             taskSource = new TaskCompletionSource<TResult>();
+        }
+
+        public async Task<TResult> RecycleAsync(TResult result)
+        {
+            var previouslyTask = taskSource.Task;
+            Recycle(result);
+            return await previouslyTask;
+        }
+
+        public async Task<TResult> RecycleAsync(Exception exception)
+        {
+            var previouslyTask = taskSource.Task;
+            Recycle(exception);
+            return await previouslyTask.WithExceptionIgnore();
         }
     }
 }
