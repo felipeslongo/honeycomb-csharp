9249f2fba0cf59c671b545cd785f6a30a14ced99
Felipe de Souza Longo <felipeslongo@gmail.com>
2020-04-28 17:30:28 -0300

    Create ConfirmationViewModel support for Android

---
+++ HoneyComb.Platform.UI.AndroidS/ConfirmationAndroidViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Android.Arch.Lifecycle;
+using HoneyComb.UI.Dialogs;
+
+namespace HoneyComb.Platform.UI.AndroidS
+{
+    public sealed class ConfirmationAndroidViewModel : ViewModel
+    {
+        public ConfirmationViewModel ViewModelCore { get; } = new ConfirmationViewModel();
+    }
+}
---
+++ HoneyComb.Platform.UI.AndroidS/ConfirmationDialogView.cs
@@ -0,0 +1,67 @@
+﻿using Android.Support.V4.App;
+using HoneyComb.Core.Lifecycles;
+
+namespace HoneyComb.Platform.UI.AndroidS
+{
+    public sealed class ConfirmationDialogView
+    {
+        private readonly FragmentActivity activity;
+        private readonly ILifecycleOwner lifecycleOwner;
+        private readonly ConfirmationAndroidViewModel viewModel;
+
+        public ConfirmationDialogView(FragmentActivity activity,
+            ILifecycleOwner lifecycleOwner,
+            ConfirmationAndroidViewModel viewModel)
+        {
+            this.activity = activity;
+            this.lifecycleOwner = lifecycleOwner;
+            this.viewModel = viewModel;
+            Init();
+        }
+
+        public string FragmentTag => typeof(ConfirmationDialogFragment).FullName;
+
+        public bool IsFragmentVisible => GetDialogFragment() != null;
+
+        private void DismissDialogFragment()
+        {
+            var currentFragment = GetDialogFragment();
+            if (currentFragment is null) return;
+
+            currentFragment.Dismiss();
+        }
+
+        private ConfirmationDialogFragment? GetDialogFragment()
+        {
+            var fragmentManager = activity.SupportFragmentManager;
+            var currentFragment = (ConfirmationDialogFragment)fragmentManager.FindFragmentByTag(FragmentTag);
+            return currentFragment;
+        }
+
+        private void Init()
+        {
+            viewModel.ViewModelCore.Visibility.Value
+                .BindMethod(lifecycleOwner, OnVisibilityChanged);
+        }
+
+        private void OnVisibilityChanged(bool isVisible)
+        {
+            if (isVisible)
+            {
+                ShowDialogFragment();
+                return;
+            }
+
+            DismissDialogFragment();
+        }
+
+        private void ShowDialogFragment()
+        {
+            if (IsFragmentVisible)
+                return;
+
+            new ConfirmationDialogFragment(viewModel)
+                .Show(activity.SupportFragmentManager, FragmentTag);
+        }
+    }
+}
