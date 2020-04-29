54e89a739d34d2e2f1df4ce279a64f2ac8661f18
Felipe de Souza Longo <felipeslongo@gmail.com>
2020-04-29 02:17:04 -0300

    Create DestroyedForRecreation for managing recreation of activity or fragment
    by the OS

---
+++ HoneyComb.Platform.Android/OS/HasBeenDestroyedForRecreation.cs
@@ -0,0 +1,26 @@
+﻿using Android.App;
+using Android.OS;
+
+namespace HoneyComb.Platform.Android.OS
+{
+    /// <summary>
+    ///     Utility to help standardize the use case of detecting
+    ///     if an Activity or Fragment has been destroyed
+    ///     by the OS for recreation.
+    /// </summary>
+    public sealed class HasBeenDestroyedForRecreation
+    {
+        public bool Value { get; private set; }
+
+        public static implicit operator bool(HasBeenDestroyedForRecreation @this) => @this.Value;
+
+        /// <summary>
+        ///     Call this on <see cref="Activity.OnRestoreInstanceState(Bundle)"/>
+        ///     or in <see cref="Fragment.OnViewStateRestored(Bundle)"/>
+        ///     or in any lifecycle method that has an SavedInstanceState bundle as parameter,
+        ///     like OnCreate
+        /// </summary>
+        public void NotifySavedInstanceState(Bundle savedInstanceState) =>
+            Value = savedInstanceState != null;
+    }
+}
