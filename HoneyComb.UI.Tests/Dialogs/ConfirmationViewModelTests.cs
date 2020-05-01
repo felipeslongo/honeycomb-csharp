3ff7fab44bad5ec8810e00a61395fbed84ebffbe
Felipe de Souza Longo <felipeslongo@gmail.com>
2020-05-01 12:12:29 -0300

    Increase test timeout in ConfirmationViewModelTest II

--- HoneyComb.UI.Tests/Dialogs/ConfirmationViewModelTests.cs
+++ HoneyComb.UI.Tests/Dialogs/ConfirmationViewModelTests.cs
@@ -87,7 +87,7 @@ namespace HoneyComb.UI.Tests
 
         public class ShowAsyncTests : ConfirmationViewModelTests
         {
-            [Fact(Timeout = 500)]
+            [Fact(Timeout = 1000)]
             [Trait(nameof(Category), Category.Unit)]
             public async Task GivenMultipleOrderedCallsToShowAsync_WhenTaskResponsesAreInvoked_ShouldReturnTheAsyncResultInTheSameCallOrder()
             {
